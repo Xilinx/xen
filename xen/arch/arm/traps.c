fd0384a54f6c101bdb785c3a118221e3fbf3d164
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2017-01-30 16:41:57 +0100

    xen/arm: Introduce platform_hvc
    Introduce platform_hvc as a way to handle hypercalls that
    Xen does not know about in a platform specific way. This
    is particularly useful for implementing the SiP (SoC
    implementation specific) service calls.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- xen/arch/arm/traps.c
+++ xen/arch/arm/traps.c
@@ -43,6 +43,7 @@
 #include <asm/cpufeature.h>
 #include <asm/flushtlb.h>
 #include <asm/monitor.h>
+#include <asm/platform.h>
 
 #include "decode.h"
 #include "vtimer.h"
@@ -1461,6 +1462,8 @@ static void do_trap_psci(struct cpu_user_regs *regs)
         }
         break;
     default:
+        if ( platform_hvc(regs) )
+            return;
         domain_crash_synchronous();
         return;
     }
