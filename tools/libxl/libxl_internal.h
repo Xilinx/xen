a05cd1f1863c6b1564f4d6642057f459d16f1450
Zhongze Liu <blackskygg@gmail.com>
2018-12-05 14:16:00 -0800

    libxl: support unmapping static shared memory areas during domain destruction
    Add libxl__sshm_del to unmap static shared memory areas mapped by
    libxl__sshm_add during domain creation. The unmapping process is:
    
    * For a owner: decrease the refcount of the sshm region, if the refcount
      reaches 0, cleanup the whole sshm path.
    
    * For a borrower:
      1) unmap the shared pages, and cleanup related xs entries. If the
         system works normally, all the shared pages will be unmapped, so there
         won't be page leaks. In case of errors, the unmapping process will go
         on and unmap all the other pages that can be unmapped, so the other
         pages won't be leaked, either.
      2) Decrease the refcount of the sshm region, if the refcount reaches
         0, cleanup the whole sshm path.
    
    This is for the proposal "Allow setting up shared memory areas between VMs
    from xl config file" (see [1]).
    
    [1] https://lists.xen.org/archives/html/xen-devel/2017-08/msg03242.html
    
    Signed-off-by: Zhongze Liu <blackskygg@gmail.com>
    Signed-off-by: Stefano Stabellini <stefanos@xilinx.com>
    
    Cc: Ian Jackson <ian.jackson@eu.citrix.com>
    Cc: Wei Liu <wei.liu2@citrix.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: xen-devel@lists.xen.org

--- tools/libxl/libxl_internal.h
+++ tools/libxl/libxl_internal.h
@@ -4846,6 +4846,8 @@ int libxl__is_domid_recent(libxl__gc *gc, uint32_t domid, bool *recent);
 _hidden int libxl__sshm_add(libxl__gc *gc, uint32_t domid,
                             libxl_static_shm *sshm, int len);
 
+_hidden int libxl__sshm_del(libxl__gc *gc, uint32_t domid);
+
 _hidden int libxl__sshm_check_overlap(libxl__gc *gc, uint32_t domid,
                                       libxl_static_shm *sshms, int len);
 _hidden int libxl__sshm_setdefault(libxl__gc *gc, uint32_t domid,
