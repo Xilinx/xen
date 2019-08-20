f98ba91f012a24241ef396d1ac7dcd25cf8560ab
Luca Miccio <206497@studenti.unimore.it>
2019-08-20 16:49:13 +0200

    xen/arm: add debug logging interface for coloring
    Signed-off-by: Luca Miccio <206497@studenti.unimore.it>
    Signed-off-by: Marco Solieri <marco.solieri@unimore.it>
    Acked-by: Stefano Stabellini <stefanos@xilinx.com>

---
+++ xen/include/asm-arm/coloring.h
@@ -0,0 +1,34 @@
+/*
+ * xen/include/asm-arm/coloring.h
+ *
+ * Coloring support for ARM
+ *
+ * Copyright (C) 2019 Xilinx Inc.
+ *
+ * Authors:
+ *    Luca Miccio <lucmiccio@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASM_ARM_COLORING_H__
+#define __ASM_ARM_COLORING_H__
+
+/* Logging utilities */
+#if defined(CONFIG_COLORING) && defined(CONFIG_COLORING_DEBUG)
+#define C_DEBUG(fmt, args...) \
+	printk(fmt, ##args)
+#else
+#define C_DEBUG(fmt, args...) { }
+#endif
+
+#endif /* !__ASM_ARM_COLORING_H__ */
