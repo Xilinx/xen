e9f4176d1367a1d247f43ad0f30927563b8e279d
Luca Miccio <206497@studenti.unimore.it>
2019-08-20 16:08:10 +0200

    xen/arm: add parsing function for cache coloring configuration
    Add three new bootargs allowing configuration of cache coloring support
    for Xen:
    - way_size: The size of a LLC way in bytes. This value is mainly used
      to calculate the maximum available colors on the platform.
    - dom0_colors: The coloring configuration for Dom0, which also acts as
      default configuration for any DomU without an explicit configuration.
    - xen_colors: The coloring configuration for the Xen hypervisor itself.
    
    A cache coloring configuration consists of a selection of colors to be
    assigned to a VM or to the hypervisor. It is represented by a set of
    ranges. Add a common function that parses a string with a
    comma-separated set of hyphen-separated ranges like "0-7,15-16" and
    returns both: the number of chosen colors, and an array containing their
    ids.
    Currently we support platforms with up to 128 colors.
    
    Signed-off-by: Luca Miccio <206497@studenti.unimore.it>
    Signed-off-by: Marco Solieri <marco.solieri@unimore.it>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

---
+++ xen/include/asm-arm/coloring.h
@@ -0,0 +1,28 @@
+/*
+ * xen/include/asm-arm/coloring.h
+ *
+ * Coloring support for ARM
+ *
+ * Copyright (C) 2019 Xilinx Inc.
+ *
+ * Authors:
+ *    Luca Miccio <lucmiccio@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASM_ARM_COLORING_H__
+#define __ASM_ARM_COLORING_H__
+
+#define MAX_COLORS_CELLS 4
+
+#endif /* !__ASM_ARM_COLORING_H__ */
