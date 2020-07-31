1ceb545d73571c882d799b65ca7f6b22d4b7f1de
Stefano Stabellini <stefano.stabellini@xilinx.com>
2020-07-31 13:40:08 -0700

    xen/arm: introduce XEN_DOMCTL_INTERNAL_directmap
    Introduce a new ARM-specific flag to specify that the domain should be
    directly mapped (guest physical addresses == physical addresses).
    
    Also, add a direct_map flag under struct arch_domain and use it to
    implement is_domain_direct_mapped.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
    CC: andrew.cooper3@citrix.com
    CC: jbeulich@suse.com
    CC: George Dunlap <George.Dunlap@eu.citrix.com>
    CC: Ian Jackson <ian.jackson@eu.citrix.com>
    CC: Wei Liu <wl@xen.org>
    CC: "Roger Pau Monné" <roger.pau@citrix.com>

--- xen/arch/arm/setup.c
+++ xen/arch/arm/setup.c
@@ -843,7 +843,7 @@ void __init start_xen(unsigned long boot_phys_offset,
     struct domain *dom0;
     struct xen_domctl_createdomain dom0_cfg = {
         .flags = XEN_DOMCTL_CDF_hvm | XEN_DOMCTL_CDF_hap |
-                 XEN_DOMCTL_INTERNAL_ispriv,
+                 XEN_DOMCTL_INTERNAL_ispriv | XEN_DOMCTL_INTERNAL_directmap,
         .max_evtchn_port = -1,
         .max_grant_frames = gnttab_dom0_frames(),
         .max_maptrack_frames = -1,
