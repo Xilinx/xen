86edb91302f5855ac09e6ab902d4c7bf5d24a801
Stefano Stabellini <stefano.stabellini@xilinx.com>
2020-07-31 13:43:08 -0700

    xen/arm: vgic: introduce vgic.cbase
    Add a field in struct vgic_dist to store the cpu interface base address,
    similar to the existing dbase field.
    
    Use the field in vgic_v2_domain_init instead of a local variable.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- xen/include/asm-arm/vgic.h
+++ xen/include/asm-arm/vgic.h
@@ -152,6 +152,7 @@ struct vgic_dist {
     struct pending_irq *pending_irqs;
     /* Base address for guest GIC */
     paddr_t dbase; /* Distributor base address */
+    paddr_t cbase; /* CPU interface base address */
 #ifdef CONFIG_GICV3
     /* GIC V3 addressing */
     /* List of contiguous occupied by the redistributors */
