7d50106b70d6c57399e7261231e48695b6bd435c
Vikram Garhwal <fnu.vikram@xilinx.com>
2021-03-22 19:09:50 -0700

    device tree: Remove init from function type
    Change handle_device_interrupt() and unflatten_dt_node() types
    
    Signed-off-by: Vikram Garhwal <fnu.vikram@xilinx.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- xen/include/asm-arm/domain_build.h
+++ xen/include/asm-arm/domain_build.h
@@ -4,10 +4,20 @@
 #include <xen/sched.h>
 #include <asm/kernel.h>
 
+struct map_range_data
+{
+    struct domain *d;
+    p2m_type_t p2mt;
+};
+
 int map_irq_to_domain(struct domain *d, unsigned int irq,
                       bool need_mapping, const char *devname);
 int make_chosen_node(const struct kernel_info *kinfo);
 void evtchn_allocate(struct domain *d);
+int handle_device_interrupts(struct domain *d, struct dt_device_node *dev,
+                             bool need_mapping);
+int map_range_to_domain(const struct dt_device_node *dev, u64 addr, u64 len,
+                        void *data);
 
 #ifndef CONFIG_ACPI
 static inline int prepare_acpi(struct domain *d, struct kernel_info *kinfo)
