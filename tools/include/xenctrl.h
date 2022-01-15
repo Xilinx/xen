94390572757bccf6ddd6f9b06e4fa848152103a4
Vikram Garhwal <fnu.vikram@xilinx.com>
2022-01-15 00:21:09 -0800

    tools/libs/ctrl: Implement new xc interfaces for dt overlay
    xc_dt_overlay() sends the device tree binary overlay, size of .dtbo and overlay
    operation type i.e. add or remove to xen.
    
    Signed-off-by: Vikram Garhwal <fnu.vikram@xilinx.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- tools/include/xenctrl.h
+++ tools/include/xenctrl.h
@@ -2692,6 +2692,9 @@ int xc_livepatch_replace(xc_interface *xch, char *name, uint32_t timeout, uint32
 int xc_domain_cacheflush(xc_interface *xch, uint32_t domid,
                          xen_pfn_t start_pfn, xen_pfn_t nr_pfns);
 
+int xc_dt_overlay(xc_interface *xch, void *overlay_fdt, int overlay_fdt_size,
+                  uint8_t overlayop);
+
 /* Compat shims */
 #include "xenctrl_compat.h"
 
