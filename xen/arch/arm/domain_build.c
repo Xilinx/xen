a9ed13eef9582253c2aa9c80e22149b52192261e
Stefano Stabellini <stefano.stabellini@xilinx.com>
2022-01-25 17:23:09 -0800

    xen: dynamically choose the gnttab region for direct-map domUs
    Direct-map dom0less guests don't have a fixed address space layout,
    instead they inherit the layout of the platform they are running on.
    
    Thus, we cannot just use the static gnttab domU region
    (GUEST_GNTTAB_BASE/GUEST_GNTTAB_SIZE) as it might conflict with
    something else residing at the same physical address.
    
    Solve the problem by reusing the same function and method to allocate
    the gnttab region already used for dom0 (find_gnttab_region).
    
    Link: https://lore.kernel.org/xen-devel/alpine.DEB.2.22.394.2201251659550.27308@ubuntu-linux-20-04-desktop/
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
    ---
    Changes in v2:
    - use is_domain_direct_mapped instead of is_hardware_domain
    - also handle the !is_domain_direct_mapped case in find_gnttab_region

--- xen/arch/arm/domain_build.c
+++ xen/arch/arm/domain_build.c
@@ -2597,8 +2597,6 @@ static int __init prepare_dtb_domU(struct domain *d, struct kernel_info *kinfo)
     int ret;
 
     kinfo->phandle_gic = GUEST_PHANDLE_GIC;
-    kinfo->gnttab_start = GUEST_GNTTAB_BASE;
-    kinfo->gnttab_size = GUEST_GNTTAB_SIZE;
 
     addrcells = GUEST_ROOT_ADDRESS_CELLS;
     sizecells = GUEST_ROOT_SIZE_CELLS;
@@ -2844,8 +2842,16 @@ static void __init find_gnttab_region(struct domain *d,
      * Only use the text section as it's always present and will contain
      * enough space for a large grant table
      */
-    kinfo->gnttab_start = __pa(_stext);
-    kinfo->gnttab_size = gnttab_dom0_frames() << PAGE_SHIFT;
+    if ( is_domain_direct_mapped(d) )
+    {
+        kinfo->gnttab_start = __pa(_stext);
+        kinfo->gnttab_size = gnttab_dom0_frames() << PAGE_SHIFT;
+    }
+    else
+    {
+        kinfo->gnttab_start = GUEST_GNTTAB_BASE;
+        kinfo->gnttab_size = GUEST_GNTTAB_SIZE;
+    }
 
 #ifdef CONFIG_ARM_32
     /*
@@ -3009,6 +3015,7 @@ static int __init construct_domU(struct domain *d,
         allocate_static_memory(d, &kinfo, node);
     else
         assign_static_memory_11(d, &kinfo, node);
+    find_gnttab_region(d, &kinfo);
 
     /*
      * Base address and irq number are needed when creating vpl011 device
