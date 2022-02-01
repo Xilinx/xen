ace9c34aba39326db9195c36c0a3a21daf3e1b97
Vikram Garhwal <fnu.vikram@xilinx.com>
2022-01-31 18:01:48 -0800

    tools/libs/light: Modify dtbo to domU linux dtbo format
    Add support for modifying dtbo to make it suitable for DomU Linux. This is
    done for '-e expert' mode.
    
    Signed-off-by: Vikram Garhwal <fnu.vikram@xilinx.com>
    Acked-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- tools/libs/light/libxl_overlay.c
+++ tools/libs/light/libxl_overlay.c
@@ -18,6 +18,7 @@
 #include <libfdt.h>
 #include <xenguest.h>
 #include <xenctrl.h>
+#include <xen/device_tree_defs.h>
 
 static int check_overlay_fdt(libxl__gc *gc, void *fdt, size_t size)
 {
@@ -40,6 +41,69 @@ static int check_overlay_fdt(libxl__gc *gc, void *fdt, size_t size)
     }
 
     return 0;
+}
+
+static int modify_overlay_for_domU(libxl__gc *gc, void *overlay_dt_domU,
+                                   size_t size)
+{
+    int rc = 0;
+    int virtual_interrupt_parent = GUEST_PHANDLE_GIC;
+    const struct fdt_property *fdt_prop_node = NULL;
+    int overlay;
+    int prop_len = 0;
+    int subnode = 0;
+    int fragment;
+    const char *prop_name;
+    const char *target_path = "/";
+
+    fdt_for_each_subnode(fragment, overlay_dt_domU, 0) {
+        prop_name = fdt_getprop(overlay_dt_domU, fragment, "target-path",
+                                &prop_len);
+        if (prop_name == NULL) {
+            LOG(ERROR, "target-path property not found\n");
+            rc = ERROR_FAIL;
+            goto err;
+        }
+
+        /* Change target path for domU dtb. */
+        rc = fdt_setprop_string(overlay_dt_domU, fragment, "target-path",
+                                target_path);
+        if (rc) {
+            LOG(ERROR, "Setting interrupt parent property failed for %s\n",
+                prop_name);
+            goto err;
+        }
+
+        overlay = fdt_subnode_offset(overlay_dt_domU, fragment, "__overlay__");
+
+        fdt_for_each_subnode(subnode, overlay_dt_domU, overlay)
+        {
+            const char *node_name = fdt_get_name(overlay_dt_domU, subnode,
+                                                 NULL);
+
+            fdt_prop_node = fdt_getprop(overlay_dt_domU, subnode,
+                                        "interrupt-parent", &prop_len);
+            if (fdt_prop_node == NULL) {
+                LOG(DETAIL, "%s property not found for %s. Skip to next node\n",
+                    "interrupt-parent", node_name);
+                continue;
+            }
+
+            rc = fdt_setprop_inplace_u32(overlay_dt_domU, subnode,
+                                         "interrupt-parent",
+                                         virtual_interrupt_parent);
+            if (rc) {
+                LOG(ERROR, "Setting interrupt parent property failed for %s\n",
+                    "interrupt-parent");
+                goto err;
+            }
+        }
+    }
+
+return 0;
+
+err:
+    return rc;
 }
 
 int libxl_dt_overlay(libxl_ctx *ctx, uint32_t domid, void *overlay_dt,
@@ -70,6 +134,15 @@ int libxl_dt_overlay(libxl_ctx *ctx, uint32_t domid, void *overlay_dt,
         LOG(ERROR, "domain%d: Adding/Removing overlay dtb failed.\n", domid);
         rc = ERROR_FAIL;
         goto out;
+    }
+
+    /*
+     * auto_mode doesn't apply to dom0 as dom0 can get the physical
+     * description of the hardware.
+     */
+    if (domid && auto_mode) {
+        if (op == LIBXL_DT_OVERLAY_ADD)
+            rc = modify_overlay_for_domU(gc, overlay_dt, overlay_dt_size);
     }
 
 out:
