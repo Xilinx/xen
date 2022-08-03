97fdfeed4b1de9028a26f6d68acf588208316614
Tanmay Shah <tanmay.shah@xilinx.com>
2022-08-03 08:56:56 -0700

    xen/eemi: Add EEMI calls to support SGI registration
    This patch adds SGI registration functionality in Xen.
    Xen registers SGI 3 with TF-A using TF_A_PM_REGISTER_SGI call.
    
    Xilinx event management driver registers SGI 15 for any hardware error
    notification. This mechanism fails when multiple VMs boot with Xen.
    This patch resolves that problem by xen receiving original SGI 3 from
    TF-A and injecting fake SGI 15 to all VMs that has registered SGI 15
    with Xen.
    
    Three SMC calls are added by this patch.
    PM_REGISTER_NOTIFIER: to register events that VM wants to be notified.
    		      This call is supported by ZU+ and Versal both platforms.
    PM_GET_CALLBACK_DATA: to get data from TF-A after SGI occurs.
    		      This call is supported by ZU+ and Versal both platforms.
    TF_A_PM_REGISTER_SGI: to register / unregister SGI from VM.
    		      This call is supported only by Versal platform.
    
    Signed-off-by: Tanmay Shah <tanmay.shah@amd.com>
    Signed-off-by: Tanmay Shah <tanmay.shah@xilinx.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/gic.c
+++ xen/arch/arm/gic.c
@@ -321,6 +321,9 @@ static void do_static_sgi(struct cpu_user_regs *regs, enum gic_sgi sgi)
     case GIC_SGI_CALL_FUNCTION:
         smp_call_function_interrupt();
         break;
+    case GIC_SGI_FIRMWARE:
+        platform_firmware_sgi();
+        break;
     default:
         panic("Unhandled SGI %d on CPU%d\n", sgi, smp_processor_id());
         break;
