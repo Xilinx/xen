3ce462be2743c252701e9f2274e52b1041f59aaa
Rahul Singh <rahul.singh@arm.com>
2022-12-01 16:02:39 +0000

    xen/arm: vsmmuv3: Emulated SMMUv3 device tree node for dom0less
    XEN will create an Emulated SMMUv3 device tree node in the device tree
    to enable the dom0less domains to discover the virtual SMMUv3 during
    boot. Emulated SMMUv3 device tree node will only be created when cmdline
    option vsmmuv3 is enabled.
    
    Signed-off-by: Rahul Singh <rahul.singh@arm.com>

--- xen/arch/arm/dom0less-build.c
+++ xen/arch/arm/dom0less-build.c
@@ -950,6 +950,49 @@ static int __init make_virtio_pci_domU_node(const struct kernel_info *kinfo)
     return res;
 }
 
+#ifdef CONFIG_VIRTUAL_ARM_SMMU_V3
+static int __init make_vsmmuv3_node(const struct kernel_info *kinfo)
+{
+    int res;
+    char buf[24];
+    __be32 reg[GUEST_ROOT_ADDRESS_CELLS + GUEST_ROOT_SIZE_CELLS];
+    __be32 *cells;
+    void *fdt = kinfo->fdt;
+
+    snprintf(buf, sizeof(buf), "iommu@%llx", GUEST_VSMMUV3_BASE);
+
+    res = fdt_begin_node(fdt, buf);
+    if ( res )
+        return res;
+
+    res = fdt_property_string(fdt, "compatible", "arm,smmu-v3");
+    if ( res )
+        return res;
+
+    /* Create reg property */
+    cells = &reg[0];
+    dt_child_set_range(&cells, GUEST_ROOT_ADDRESS_CELLS, GUEST_ROOT_SIZE_CELLS,
+                       GUEST_VSMMUV3_BASE, GUEST_VSMMUV3_SIZE);
+    res = fdt_property(fdt, "reg", reg,
+                       (GUEST_ROOT_ADDRESS_CELLS +
+                       GUEST_ROOT_SIZE_CELLS) * sizeof(*reg));
+    if ( res )
+        return res;
+
+    res = fdt_property_cell(fdt, "phandle", GUEST_PHANDLE_VSMMUV3);
+    if ( res )
+        return res;
+
+    res = fdt_property_cell(fdt, "#iommu-cells", 1);
+    if ( res )
+        return res;
+
+    res = fdt_end_node(fdt);
+
+    return res;
+}
+#endif
+
 /*
  * The max size for DT is 2MB. However, the generated DT is small (not including
  * domU passthrough DT nodes whose size we account separately), 16KB are enough
@@ -1067,6 +1110,15 @@ static int __init prepare_dtb_domU(struct domain *d, struct kernel_info *kinfo)
         if ( ret )
             goto err;
     }
+
+#ifdef CONFIG_VIRTUAL_ARM_SMMU_V3
+    if ( is_viommu_enabled() )
+    {
+        ret = make_vsmmuv3_node(kinfo);
+        if ( ret )
+            goto err;
+    }
+#endif
 
     ret = fdt_end_node(kinfo->fdt);
     if ( ret < 0 )
