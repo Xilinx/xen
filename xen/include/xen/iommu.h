43e47313e082bc689868271ff826971e0204e677
Rahul Singh <rahul.singh@arm.com>
2022-12-01 16:02:35 +0000

    xen/arm: vsmmuv3: Attach Stage-1 configuration to SMMUv3 hardware
    Attach the Stage-1 configuration to device STE to support nested
    translation for the guests.
    
    Signed-off-by: Rahul Singh <rahul.singh@arm.com>

--- xen/include/xen/iommu.h
+++ xen/include/xen/iommu.h
@@ -264,6 +264,15 @@ static inline int iommu_add_pci_sideband_ids(struct pci_dev *pdev)
     return ret;
 }
 
+#ifdef CONFIG_ARM
+struct iommu_guest_config {
+    paddr_t     s1ctxptr;
+    uint8_t     config;
+    uint8_t     s1fmt;
+    uint8_t     s1cdmax;
+};
+#endif /* CONFIG_ARM */
+
 struct page_info;
 
 /*
@@ -340,6 +349,11 @@ struct iommu_ops {
 #endif
     /* Inhibit all interrupt generation, to be used at shutdown. */
     void (*quiesce)(void);
+
+#ifdef CONFIG_ARM
+    int (*attach_guest_config)(struct domain *d, u32 sid,
+                               struct iommu_guest_config *cfg);
+#endif
 };
 
 /*
