a200fdca4eca56ff01f86fa429cda64b9b42daf6
Rahul Singh <rahul.singh@arm.com>
2022-12-01 16:02:36 +0000

    xen/arm: vsmmuv3: Add support for event queue and global error
    Event queue is used to send the events to guest when there is an events/
    faults. Add support for event queue to send events to guest.
    
    Global error in SMMUv3 hw will be updated in smmu_gerror and
    smmu_gerrorn register. Add support for global error registers to send
    global error to guest.
    
    Signed-off-by: Rahul Singh <rahul.singh@arm.com>

--- xen/include/public/arch-arm.h
+++ xen/include/public/arch-arm.h
@@ -551,6 +551,11 @@ typedef uint64_t xen_callback_t;
 #define GUEST_FFA_NOTIF_PEND_INTR_ID      8
 #define GUEST_FFA_SCHEDULE_RECV_INTR_ID   9
 
+#define GUEST_VIRTIO_PCI_SPI_FIRST   44
+#define GUEST_VIRTIO_PCI_SPI_LAST    48
+
+#define GUEST_VSMMU_SPI         49
+
 /* PSCI functions */
 #define PSCI_cpu_suspend 0
 #define PSCI_cpu_off     1
