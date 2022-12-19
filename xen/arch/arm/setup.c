Xilinx/xen

commit c8746f76f3edd8601e16c7b0422b337f0c4fd033

xen/arm: disable warnings at boot about SILO and sync_console
Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

xen/arch/arm/setup.c
@@ -1094,12 +1094,6 @@ void __init start_xen(unsigned long boot_phys_offset,
 
     tasklet_subsys_init();
 
-    if ( xsm_dt_init() != 1 )
-        warning_add("WARNING: SILO mode is not enabled.\n"
-                    "It has implications on the security of the system,\n"
-                    "unless the communications have been forbidden between\n"
-                    "untrusted domains.\n");
-
     init_maintenance_interrupt();
     init_timer_interrupt();
 

xen/drivers/char/console.c
@@ -56,12 +56,6 @@ string_runtime_param("conswitch", opt_conswitch);
 /* sync_console: force synchronous console output (useful for debugging). */
 static bool_t __initdata opt_sync_console;
 boolean_param("sync_console", opt_sync_console);
-static const char __initconst warning_sync_console[] =
-    "WARNING: CONSOLE OUTPUT IS SYNCHRONOUS\n"
-    "This option is intended to aid debugging of Xen by ensuring\n"
-    "that all output is synchronously delivered on the serial line.\n"
-    "However it can introduce SIGNIFICANT latencies and affect\n"
-    "timekeeping. It is NOT recommended for production use!\n";
 
 /* console_to_ring: send guest (incl. dom 0) console data to console ring. */
 static bool_t __read_mostly opt_console_to_ring;
@@ -1017,7 +1011,6 @@ void __init console_init_preirq(void)
         serial_start_sync(sercon_handle);
         add_taint(TAINT_SYNC_CONSOLE);
         printk("Console output is synchronous.\n");
-        warning_add(warning_sync_console);
     }
 }
 

commit 90b970ed85076143aaa72c4ba8d468d3a1cd3738

xen/arm: Invoke xsm initialization during start_xen()
XSM needs to be initialized during startup. Else, a crash is observed
when 'CONFIG_XSM=y'

Fixes: c8746f76f3ed ("xen/arm: disable warnings at boot about SILO and sync_console")

Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
Signed-off-by: Michal Orzel <michal.orzel@amd.com>
Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/arch/arm/setup.c
@@ -1094,6 +1094,10 @@ void __init start_xen(unsigned long boot_phys_offset,
 
     tasklet_subsys_init();
 
+    rc = xsm_dt_init();
+    if ( rc < 0 )
+        panic("XSM initialization failed (error %d)\n", rc);
+
     init_maintenance_interrupt();
     init_timer_interrupt();
 