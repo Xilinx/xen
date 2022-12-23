a43eb377a73f8e201bf33e0c0ce240cf7a7d1584
Michal Orzel <michal.orzel@amd.com>
2022-12-23 10:20:55 +0100

    xen/arm: vIOMMU: Add proper handling for dom0less domUs
    Introduce viommu device-tree property to enable/disable support for
    virtual IOMMU for the domain.
    
    Signed-off-by: Michal Orzel <michal.orzel@amd.com>

--- xen/arch/arm/dom0less-build.c
+++ xen/arch/arm/dom0less-build.c
@@ -1033,6 +1033,27 @@ static int __init make_vsmmuv3_node(const struct kernel_info *kinfo)
 }
 #endif
 
+static int __init make_viommu_domU_node(const struct kernel_info *kinfo)
+{
+#ifdef CONFIG_VIRTUAL_IOMMU
+    /* List head is NULL if vIOMMU was not enabled for the domain */
+    if ( list_head_is_null(&kinfo->d->arch.viommu_list) )
+        return 0;
+#endif
+
+    switch ( viommu_get_type() )
+    {
+#ifdef CONFIG_VIRTUAL_ARM_SMMU_V3
+    case XEN_DOMCTL_CONFIG_VIOMMU_SMMUV3:
+        return make_vsmmuv3_node(kinfo);
+#endif
+    case XEN_DOMCTL_CONFIG_VIOMMU_NONE:
+        return 0;
+    default:
+        panic("Unsupported vIOMMU type\n");
+    }
+}
+
 /*
  * The max size for DT is 2MB. However, the generated DT is small (not including
  * domU passthrough DT nodes whose size we account separately), 16KB are enough
@@ -1151,14 +1172,9 @@ static int __init prepare_dtb_domU(struct domain *d, struct kernel_info *kinfo)
             goto err;
     }
 
-#ifdef CONFIG_VIRTUAL_ARM_SMMU_V3
-    if ( viommu_enabled  )
-    {
-        ret = make_vsmmuv3_node(kinfo);
-        if ( ret )
-            goto err;
-    }
-#endif
+    ret = make_viommu_domU_node(kinfo);
+    if ( ret )
+        goto err;
 
     ret = fdt_end_node(kinfo->fdt);
     if ( ret < 0 )
@@ -1572,7 +1588,7 @@ void __init create_domUs(void)
         domid_t domid;
         struct xen_domctl_createdomain d_cfg = {
             .arch.gic_version = XEN_DOMCTL_CONFIG_GIC_NATIVE,
-            .arch.viommu_type = viommu_get_type(),
+            .arch.viommu_type = XEN_DOMCTL_CONFIG_VIOMMU_NONE,
             .flags = XEN_DOMCTL_CDF_hvm | XEN_DOMCTL_CDF_hap,
             /*
              * The default of 1023 should be sufficient for guests because
@@ -1748,6 +1764,9 @@ void __init create_domUs(void)
 
         if ( is_pci_scan_enabled() )
             d_cfg.flags |= XEN_DOMCTL_CDF_vpci;
+
+        if ( dt_property_read_bool(node, "viommu") )
+            d_cfg.arch.viommu_type = viommu_get_type();
 
         /*
          * The variable max_init_domid is initialized with zero, so here it's
