09ea7e6c07057d380e78cab4c65d82b58db76fe2
Stefano Stabellini <stefano.stabellini@amd.com>
2022-12-23 09:48:44 -0800

    xen/arm: support specifying viommu type in device tree
    In addition to leaving the property empty to select the default.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/domain_build.c
+++ xen/arch/arm/domain_build.c
@@ -3968,6 +3968,7 @@ void __init create_domUs(void)
     struct dt_device_node *node;
     const struct dt_device_node *cpupool_node,
                                 *chosen = dt_find_node_by_path("/chosen");
+    const char *viommu_str;
 
     BUG_ON(chosen == NULL);
     dt_for_each_child_node(chosen, node)
@@ -4107,8 +4108,16 @@ void __init create_domUs(void)
 #endif
         }
 
-        if ( dt_property_read_bool(node, "viommu") )
+        rc = dt_property_read_string(node, "viommu", &viommu_str);
+        if ( rc == -ENODATA )
             d_cfg.arch.viommu_type = viommu_get_type();
+        else if ( !rc )
+        {
+            if ( !strcmp(viommu_str, "smmuv3") )
+                d_cfg.arch.viommu_type = XEN_DOMCTL_CONFIG_VIOMMU_SMMUV3;
+            else
+                panic("Unknown vIOMMU %s\n", viommu_str);
+        }
 
         /*
          * The variable max_init_domid is initialized with zero, so here it's
