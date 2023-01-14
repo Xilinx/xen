Xilinx/xen

commit 9f745bf9bbaf1b20802d8a3456862c467425a2bf

xen/arm: AArch32_V8R: Use 32 bits for the physical address
In the case of MPU (unlike MMU), there is a 1-1 mapping of virtual address
to physical address (ie VA = PA).

However, the physical addresses defined for aarch32 is u64. This is a problem
for aarch32 mpu systems as the physical addresses = virtual addresses = 32 bits.

Thus, when the memory region for FDT is mapped, it returns the virtual address
(which is the same as physical address but truncated upto the lower 32 bits).
Similar logic has been used to convert machine address to virtual address and
for ioremap as well.

We do not support physical addresses beyond 32 bits.
As this logic will fail when FDT physical address is more than 32 bits, we have
added a BUG() to catch this.

Thus, the following functions has been adapted :-
early_fdt_map()
maddr_to_virt()
ioremap_attr()

Also disable "BUILD_BUG_ON(PADDR_BITS >= BITS_PER_LONG)" as PADDR_BITS = 40
and BITS_PER_LONG = 32.

Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>
Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/arch/arm_mpu/mm_mpu.c
@@ -377,7 +377,10 @@ void *ioremap_attr(paddr_t pa, size_t len, unsigned int attributes)
         return NULL;
     }
 
-    return (void *)pa;
+    ASSERT(pa < (1ULL << PADDR_BITS));
+
+    /* In MPU system, VA == PA. */
+    return (void *) ((unsigned long) pa);
 }
 
 static void clear_boot_mpumap(void)
@@ -884,8 +887,10 @@ void * __init early_fdt_map(paddr_t fdt_paddr)
     next_xen_mpumap_index++;
     nr_xen_mpumap++;
 
-    /* VA == PA */
-    fdt_virt = (void *)fdt_paddr;
+    ASSERT(fdt_paddr < (1ULL << PADDR_BITS));
+
+    /* In MPU system, VA == PA. */
+    fdt_virt = (void *) ((unsigned long) fdt_paddr);
 
     if ( fdt_magic(fdt_virt) != FDT_MAGIC )
         return NULL;

xen/arch/arm_mpu/include/asm/mm_mpu.h
@@ -80,8 +80,10 @@ static inline paddr_t __virt_to_maddr(vaddr_t va)
 
 static inline void *maddr_to_virt(paddr_t ma)
 {
+    ASSERT(ma < (1ULL << PADDR_BITS));
+
     /* In MPU system, VA == PA. */
-    return (void *)ma;
+    return (void *) ((unsigned long) ma);
 }
 
 /*

xen/arch/arm_mpu/include/asm/page-bits.h
@@ -5,6 +5,8 @@
 
 #ifdef CONFIG_ARM_64
 #define PADDR_BITS              48
+#elif CONFIG_AARCH32_V8R
+#define PADDR_BITS              32
 #else
 #define PADDR_BITS              40
 #endif

xen/common/page_alloc.c
@@ -2444,7 +2444,9 @@ void __init xenheap_max_mfn(unsigned long mfn)
 {
     ASSERT(!first_node_initialised);
     ASSERT(!xenheap_bits);
+#ifndef CONFIG_AARCH32_V8R
     BUILD_BUG_ON(PADDR_BITS >= BITS_PER_LONG);
+#endif
     xenheap_bits = min(flsl(mfn + 1) - 1 + PAGE_SHIFT, PADDR_BITS);
     printk(XENLOG_INFO "Xen heap: %u bits\n", xenheap_bits);
 }

commit 3b77ccbe01531e4a11a9aa3b0609b46054aa6e2d

xen/arm: V8R: Fix a compilation failure
arch/arm_mpu/mm_mpu.c:1143:33: error: cast from pointer to integer of different size [-Werror=pointer-to-int-cast]
 1143 |     copy_from_paddr(xen_mpumap, (paddr_t)(pr_t *)boot_mpumap,

The pointers are of type 'unsigned long' on Arm32 architecture. Here
we are typecasting it to paddr_t ie u64. So, the compilation fails.

We should typecast it to 'unsigned long'.

Fixes: 9f745bf9bb xen/arm: AArch32_V8R: Use 32 bits for the physical address
Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/arch/arm_mpu/mm_mpu.c
@@ -1140,7 +1140,7 @@ static int __init relocate_xen_mpumap(void)
     if ( !xen_mpumap )
         return -EINVAL;
 
-    copy_from_paddr(xen_mpumap, (paddr_t)(pr_t *)boot_mpumap,
+    copy_from_paddr(xen_mpumap, (unsigned long)(pr_t *)boot_mpumap,
                     sizeof(pr_t) * next_xen_mpumap_index);
 
     clear_boot_mpumap();