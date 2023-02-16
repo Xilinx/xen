2e25e6e7ef79a49c8f4d17d954a948878a8dfeaf
Ayan Kumar Halder <ayan.kumar.halder@amd.com>
2023-02-16 12:38:23 +0000

    xen/arm: AArch32_V8R: Introduce VERSAL_NET option
    Use VERSAL_NET to initialize versal-net platform configuration.
    
    Configured CNTFRQ in the early boot stage as it being read in the later boot
    stage to obtain the timer frequency.
    
    Introduced xen/arch/arm_mpu/platforms/amd-versal-net.c for the versal-net specific
    initialization.
    
    Also, took the opportunity to remove fvp_baser.h and move the macros to
    config_mpu.h as none of these macros are FVP specific.
    
    Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
    Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm_mpu/include/asm/config_mpu.h
+++ xen/arch/arm_mpu/include/asm/config_mpu.h
@@ -7,10 +7,6 @@
 #ifndef __ARM_CONFIG_MPU_H__
 #define __ARM_CONFIG_MPU_H__
 
-#ifdef CONFIG_FVP_BASER
-#include <asm/platforms/fvp_baser.h>
-#endif
-
 /*
  * All MPU platforms need to provide a XEN_START_ADDRESS for linker. This
  * address indicates where Xen image will be loaded and run from. This
@@ -20,9 +16,13 @@
 #error "XEN_START_ADDRESS must be aligned to PAGE_SIZE"
 #endif
 
-#define XEN_VIRT_START         _AT(paddr_t, XEN_START_ADDRESS)
+#define XEN_VIRT_START         _AT(paddr_t, CONFIG_XEN_START_ADDRESS)
 
 #define HYPERVISOR_VIRT_START  XEN_VIRT_START
+
+#ifdef CONFIG_EARLY_PRINTK
+#define EARLY_UART_SIZE         0x1000
+#endif
 
 #define FRAMETABLE_SIZE        GB(32)
 #define FRAMETABLE_NR          (FRAMETABLE_SIZE / sizeof(*frame_table))
