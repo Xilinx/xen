Xilinx/xen

commit 32fcc702718591270e5c8928b7687d853249c882

libs: light: Add virtio-net support
Now that QEMU xenpv machine has virtio-net support available. Also update the
tools, add support for creating virtio-net interface for PV domains using
'vif = [ 'model=virtio-net,type=ioemu' ]' config option.

Signed-off-by: Vikram Garhwal <vikram.garhwal@amd.com>
Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>
Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

tools/libs/light/libxl_nic.c
@@ -102,6 +102,9 @@ static int libxl__device_nic_setdefault(libxl__gc *gc, uint32_t domid,
         }
         break;
     case LIBXL_DOMAIN_TYPE_PVH:
+        if (nic->model != NULL && !nic->nictype)
+            nic->nictype = LIBXL_NIC_TYPE_VIF_IOEMU;
+        break;
     case LIBXL_DOMAIN_TYPE_PV:
         if (nic->nictype == LIBXL_NIC_TYPE_VIF_IOEMU) {
             LOGD(ERROR, domid,
@@ -260,6 +263,10 @@ static int libxl__set_xenstore_nic(libxl__gc *gc, uint32_t domid,
     flexarray_append(front, "trusted");
     flexarray_append(front, libxl_defbool_val(nic->trusted) ? "1" : "0");
 
+    if (nic->model != NULL && !strcmp(nic->model, "virtio-net")) {
+        flexarray_append_pair(back, "base", GCSPRINTF("%"PRIu64, nic->base));
+        flexarray_append_pair(back, "irq", GCSPRINTF("%u", nic->irq));
+    }
     return 0;
 }
 

docs/man/xl-network-configuration.5.pod.in
@@ -103,7 +103,8 @@ NOTE: netdev is a deprecated alias of this option.
 
 =head2 type
 
-This keyword is valid for HVM guests only.
+This keyword is valid only for HVM and PVH guests. For PVH, this keyword is valid only with
+model "virtio-net".
 
 Specifies the type of device to valid values are:
 
@@ -128,7 +129,8 @@ only.
 
 =head2 model
 
-This keyword is valid for HVM guest devices with C<type=ioemu> only.
+This keyword is valid for HVM guest devices with C<type=ioemu> only. For PVH
+guest, this is supported only with C<type=ioemu>.
 
 Specifies the type device to emulated for this guest. Valid values
 are:
@@ -143,6 +145,11 @@ C<rtl8139> (default) -- Realtek RTL8139
 =item *
 
 C<e1000> -- Intel E1000
+
+
+=item *
+
+C<virtio-net> -- QEMU emulated Virtio-network device.
 
 
 =item *

tools/libs/light/libxl_arm.c
@@ -95,6 +95,29 @@ int libxl__arch_domain_prepare_config(libxl__gc *gc,
         }
     }
 
+    for (i = 0; i < d_config->num_nics; i++) {
+        libxl_device_nic *nic = &d_config->nics[i];
+
+        if (nic->model != NULL && !strcmp(nic->model, "virtio-net")) {
+            nic->base = alloc_virtio_mmio_base(gc, &virtio_mmio_base);
+            if (nic->base < GUEST_VIRTIO_MMIO_BASE) {
+                LOG(ERROR, "No available virtio-mmio\n");
+                return ERROR_FAIL;
+            }
+
+            nic->irq = alloc_virtio_mmio_irq(gc, &virtio_mmio_irq);
+            if (nic->irq < GUEST_VIRTIO_MMIO_SPI_FIRST) {
+                LOG(ERROR, "No available Virtio MMIO IRQ\n");
+                return ERROR_FAIL;
+            }
+
+            virtio_enabled = true;
+
+            LOG(DEBUG, "Allocate Virtio MMIO params for IRQ %u BASE 0x%"PRIx64,
+                nic->irq, nic->base);
+        }
+    }
+
     /*
      * Every virtio-mmio device uses one emulated SPI. If Virtio devices are
      * present, make sure that we allocate enough SPIs for them.
@@ -1444,6 +1467,21 @@ next_resize:
 
                 FDT( make_virtio_mmio_node(gc, fdt, disk->base, disk->irq,
                                            disk->backend_domid) );
+            }
+        }
+
+        for (i = 0; i < d_config->num_nics; i++) {
+            libxl_device_nic *nic = &d_config->nics[i];
+
+            if (nic->model != NULL && !strcmp(nic->model, "virtio-net")) {
+                if (nic->backend_domid != LIBXL_TOOLSTACK_DOMID &&
+                    !iommu_created) {
+                    FDT( make_xen_iommu_node(gc, fdt) );
+                    iommu_created = true;
+                }
+
+                FDT( make_virtio_mmio_node(gc, fdt, nic->base, nic->irq,
+                                           nic->backend_domid) );
             }
         }
 

tools/libs/light/libxl_dm.c
@@ -1362,8 +1362,6 @@ static int libxl__build_device_model_args_new(libxl__gc *gc,
     }
 
     if (b_info->type == LIBXL_DOMAIN_TYPE_HVM) {
-        int ioemu_nics = 0;
-
         if (b_info->kernel)
             flexarray_vappend(dm_args, "-kernel", b_info->kernel, NULL);
 
@@ -1565,17 +1563,35 @@ static int libxl__build_device_model_args_new(libxl__gc *gc,
             } else
                 flexarray_append(dm_args, GCSPRINTF("%d", b_info->max_vcpus));
         }
+    } else {
+        if (!sdl && !vnc) {
+            flexarray_append(dm_args, "-nographic");
+        }
+    }
+
+    if (b_info->type != LIBXL_DOMAIN_TYPE_PV) {
+        int ioemu_nics = 0;
+
         for (i = 0; i < num_nics; i++) {
             if (nics[i].nictype == LIBXL_NIC_TYPE_VIF_IOEMU) {
+                char *model;
                 char *smac = GCSPRINTF(LIBXL_MAC_FMT,
                                        LIBXL_MAC_BYTES(nics[i].mac));
                 const char *ifname = libxl__device_nic_devname(gc,
                                                 guest_domid, nics[i].devid,
                                                 LIBXL_NIC_TYPE_VIF_IOEMU);
+
+                /* assume PVH uses virtio-mmio for now */
+                if (b_info->type == LIBXL_DOMAIN_TYPE_PVH &&
+                    nics[i].model != NULL && !strcmp(nics[i].model, "virtio-net"))
+                    model = "virtio-net-device";
+                else
+                    model = nics[i].model;
+
                 flexarray_append(dm_args, "-device");
                 flexarray_append(dm_args,
                    GCSPRINTF("%s,id=nic%d,netdev=net%d,mac=%s",
-                             nics[i].model, nics[i].devid,
+                             model, nics[i].devid,
                              nics[i].devid, smac));
                 flexarray_append(dm_args, "-netdev");
                 flexarray_append(dm_args,
@@ -1733,10 +1749,6 @@ static int libxl__build_device_model_args_new(libxl__gc *gc,
         if ( ioemu_nics == 0 ) {
             flexarray_append(dm_args, "-net");
             flexarray_append(dm_args, "none");
-        }
-    } else {
-        if (!sdl && !vnc) {
-            flexarray_append(dm_args, "-nographic");
         }
     }
 
@@ -3799,6 +3811,22 @@ int libxl__need_xenpv_qemu(libxl__gc *gc, libxl_domain_config *d_config)
         libxl_defbool_val(d_config->b_info.tpm)) {
         ret = 1;
         goto out;
+    }
+
+    for (i = 0; i < d_config->num_disks; i++) {
+        libxl_device_disk *disk = &d_config->disks[i];
+        if (disk->specification == LIBXL_DISK_SPECIFICATION_VIRTIO) {
+            ret = 1;
+            goto out;
+        }
+    }
+
+    for (i = 0; i < d_config->num_nics; i++) {
+        libxl_device_nic *nic = &d_config->nics[i];
+        if (nic->nictype == LIBXL_NIC_TYPE_VIF_IOEMU) {
+            ret = 1;
+            goto out;
+        }
     }
 
     for (idx = 0;; idx++) {

tools/libs/light/libxl_types.idl
@@ -764,6 +764,10 @@ libxl_device_nic = Struct("device_nic", [
     ("rate_bytes_per_interval", uint64),
     ("rate_interval_usecs", uint32),
     ("gatewaydev", string),
+    # Note that virtio-mmio parameters (irq and base) are for internal use
+    # by libxl and can't be modified.
+    ("irq", uint32),
+    ("base", uint64),
     # Note that the COLO configuration settings should be considered unstable.
     # They may change incompatibly in future versions of Xen.
     ("coloft_forwarddev", string),

commit d1258f1cefe406a3f91237b8106746c089864651

libxl: default nic type should be vif
The commit ("libs: light: Add virtio-net support") changed the default vif type
to ioemu. As a consequence, if the user does not specify the nic type in the xl
config file, domU creation would fail due to incorrect device model defaults. To
successfully create a domU, it was required to specify type=vif in the xl config
file:

vif = [ 'type=vif' ]

However, omitting the type is a common use case. The default type should be vif,
not ioemu. This patch sets the default nic type to vif again. We can now again
create domUs with a regular vif by omitting the type:

vif = [ '' ]

Or, to create a virtio-net device:

vif = [ 'model=virtio-net,type=ioemu' ]

Fixes: 32fcc7027185 ("libs: light: Add virtio-net support")
Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
Reviewed-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>

tools/libs/light/libxl_nic.c
@@ -102,9 +102,9 @@ static int libxl__device_nic_setdefault(libxl__gc *gc, uint32_t domid,
         }
         break;
     case LIBXL_DOMAIN_TYPE_PVH:
-        if (nic->model != NULL && !nic->nictype)
-            nic->nictype = LIBXL_NIC_TYPE_VIF_IOEMU;
-        break;
+        if (nic->nictype == LIBXL_NIC_TYPE_VIF_IOEMU)
+            break;
+        /* else fall through */
     case LIBXL_DOMAIN_TYPE_PV:
         if (nic->nictype == LIBXL_NIC_TYPE_VIF_IOEMU) {
             LOGD(ERROR, domid,