9f4b6d3810d70dbebf4b92e53ca08a065dc9ab72
Michal Orzel <michal.orzel@amd.com>
2023-07-07 10:33:11 +0200

    xen/arm: vpl011: Add sbsa parameter to domain_vpl011_init()
    We need to have a way to inform vpl011 driver (used to emulate SBSA UART
    and soon PL011) the vpl011 emulation type as described by enum
    vpl011_type.
    
    Signed-off-by: Michal Orzel <michal.orzel@amd.com>
    Reviewed-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
    Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/domctl.c
+++ xen/arch/arm/domctl.c
@@ -41,7 +41,7 @@ static int handle_vuart_init(struct domain *d,
     if ( vuart_op->type != XEN_DOMCTL_VUART_TYPE_SBSA_UART )
         return -EOPNOTSUPP;
 
-    rc = domain_vpl011_init(d, &info);
+    rc = domain_vpl011_init(d, &info, true);
 
     if ( !rc )
         vuart_op->evtchn = info.evtchn;
