2ebe229462a7c255c7c3217fe47e2cabefb6bd05
Luca Fancellu <luca.fancellu@arm.com>
2024-02-19 12:19:16 -0500

    xen/pci: add discovered PCI device at boot
    Create add_discovered_pci_devices function that calls pci_device_add
    on every PCI device discovered.
    The devices will be added to dom_io so that they can be assigned
    later to other domains.
    
    Issue-Id: SCM-1012
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Change-Id: Ia398a331840ce7299ada266214c1052f94082e54
    Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/pci/pci.c
+++ xen/arch/arm/pci/pci.c
@@ -111,6 +111,8 @@ static int __init pci_init(void)
         if ( ret < 0 )
             return ret;
 
+        add_discovered_pci_devices();
+
 #ifdef CONFIG_PCI_DEVICE_DEBUG
         dump_pci_devices('c');
 #endif
--- xen/include/xen/pci.h
+++ xen/include/xen/pci.h
@@ -177,6 +177,7 @@ bool pdev_list_is_read_locked(const struct domain *d);
 bool pci_known_segment(u16 seg);
 bool pci_device_detect(u16 seg, u8 bus, u8 dev, u8 func);
 int scan_pci_devices(void);
+void add_discovered_pci_devices(void);
 void dump_pci_devices(unsigned char ch);
 enum pdev_type pdev_type(u16 seg, u8 bus, u8 devfn);
 int find_upstream_bridge(u16 seg, u8 *bus, u8 *devfn, u8 *secbus);
