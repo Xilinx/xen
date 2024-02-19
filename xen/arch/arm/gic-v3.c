0bdf1f2869f3788e7ab98d5ef53e00737c1be890
Luca Fancellu <luca.fancellu@arm.com>
2024-02-19 12:19:21 -0500

    xen/pci: update DT for hwdom when it uses vpci
    When hwdom_uses_vpci() is true, the device tree for the hardware
    domain is modified in this way:
     - Nodes with device_type equal to "pci" are removed
     - A vpci node is created for the hwdom device tree
     - The interrupt controller node will have the emulated ITS node.
    
    Add ranges property to the gicv3 device tree node for hwdom.
    
    Issue-Id: SCM-1012
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Change-Id: If91d7c7bf1914489b14c4d6dd70ba78dfa8f38d5
    Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/gic-v3.c
+++ xen/arch/arm/gic-v3.c
@@ -1305,6 +1305,9 @@ static int gicv3_make_hwdom_dt_node(const struct domain *d,
     if ( res )
         return res;
 
+    if ( hwdom_uses_vpci() )
+        return gicv3_its_make_emulated_dt_node(fdt);
+
     return gicv3_its_make_hwdom_dt_nodes(d, gic, fdt);
 }
 
