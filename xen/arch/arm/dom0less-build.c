Xilinx/xen

commit 4e710bee0e89a75fa46b40fe95ac2be6a25b53e7

xen: dynamically choose the gnttab region for direct-map domUs
Direct-map dom0less guests don't have a fixed address space layout,
instead they inherit the layout of the platform they are running on.

Thus, we cannot just use the static gnttab domU region
(GUEST_GNTTAB_BASE/GUEST_GNTTAB_SIZE) as it might conflict with
something else residing at the same physical address.

Solve the problem by reusing the same function and method to allocate
the gnttab region already used for dom0 (find_gnttab_region).

Link: https://lore.kernel.org/xen-devel/alpine.DEB.2.22.394.2201251659550.27308@ubuntu-linux-20-04-desktop/
Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
[Michal: use domain_use_host_layout to properly handle LLC coloring case]
Signed-off-by: Michal Orzel <michal.orzel@amd.com>

xen/arch/arm/dom0less-build.c
@@ -894,8 +894,6 @@ static int __init prepare_dtb_domU(struct domain *d, struct kernel_info *kinfo)
     int ret, fdt_size = DOMU_DTB_SIZE;
 
     kinfo->phandle_gic = GUEST_PHANDLE_GIC;
-    kinfo->gnttab_start = GUEST_GNTTAB_BASE;
-    kinfo->gnttab_size = GUEST_GNTTAB_SIZE;
 
     addrcells = GUEST_ROOT_ADDRESS_CELLS;
     sizecells = GUEST_ROOT_SIZE_CELLS;

xen/arch/arm/domain_build.c
@@ -2343,8 +2343,16 @@ static void __init find_gnttab_region(struct domain *d,
      * Only use the text section as it's always present and will contain
      * enough space for a large grant table
      */
-    kinfo->gnttab_start = __pa(_stext);
-    kinfo->gnttab_size = gnttab_dom0_frames() << PAGE_SHIFT;
+    if ( domain_use_host_layout(d) )
+    {
+        kinfo->gnttab_start = __pa(_stext);
+        kinfo->gnttab_size = gnttab_dom0_frames() << PAGE_SHIFT;
+    }
+    else
+    {
+        kinfo->gnttab_start = GUEST_GNTTAB_BASE;
+        kinfo->gnttab_size = GUEST_GNTTAB_SIZE;
+    }
 
 #ifdef CONFIG_ARM_32
     /*

commit 9bd07e80da69dfa34b7a51b19e758174d5ddbdda

xen/arm: Fix gnttab region setting for dom0less domUs
Commit 4e710bee0e89 accidentally removed setting of gnttab region for
dom0less domUs. This results in the region being empty. Fix it.

CR: CR-1240197
Fixes: 4e710bee0e89 ("xen: dynamically choose the gnttab region for direct-map domUs")
Signed-off-by: Michal Orzel <michal.orzel@amd.com>
Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/arch/arm/dom0less-build.c
@@ -1583,6 +1583,8 @@ static int __init construct_domU(struct domain *d,
     }
     else
     {
+        find_gnttab_region(d, &kinfo);
+
         if ( !dt_find_property(node, "xen,static-mem", NULL) )
             allocate_memory(d, &kinfo);
         else if ( !is_domain_direct_mapped(d) )

xen/arch/arm/domain_build.c
@@ -2443,8 +2443,7 @@ void __init evtchn_allocate(struct domain *d)
     d->arch.hvm.params[HVM_PARAM_CALLBACK_IRQ] = val;
 }
 
-static void __init find_gnttab_region(struct domain *d,
-                                      struct kernel_info *kinfo)
+void __init find_gnttab_region(struct domain *d, struct kernel_info *kinfo)
 {
     /*
      * The region used by Xen on the memory will never be mapped in DOM0

xen/arch/arm/include/asm/domain_build.h
@@ -63,6 +63,7 @@ int prepare_acpi(struct domain *d, struct kernel_info *kinfo);
 #endif
 
 int add_ext_regions(unsigned long s_gfn, unsigned long e_gfn, void *data);
+void find_gnttab_region(struct domain *d, struct kernel_info *kinfo);
 
 #endif
 