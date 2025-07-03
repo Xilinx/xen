c699274b178149aeaf33633bd9db96f882fd13f6
Alejandro Vallejo <alejandro.garciavallejo@amd.com>
2025-07-03 18:58:28 +0200

    x86/domain-builder: Support "reg" props in xen,domain nodes' blobs
    In exotic configurations it's possible to pass modules in specific
    areas of the initial system memory. Allow modules to be described
    by "reg" properties, and add those modules once parsed to the list of
    modules in boot_info for tracking purposes, with care not to add the
    same module multiple times.
    
    Signed-off-by: Alejandro Vallejo <alejandro.garciavallejo@amd.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>
    Reviewed-by: Jason Andryuk <jason.andryuk@amd.com>

--- xen/arch/x86/domain-builder/dtb.c
+++ xen/arch/x86/domain-builder/dtb.c
@@ -13,19 +13,63 @@ static int __init cf_check process_module(const void *fdt, int node,
 {
     struct boot_info *bi = data;
     uint32_t idx;
+    const struct fdt_property *prop;
+    paddr_t start, size;
+    const __be32 *cell;
+    int len;
 
     if ( fdt_node_check_compatible(fdt, node, "multiboot,module") )
         return 0;
 
-    idx = device_tree_get_u32(fdt, node, "module-index", MAX_NR_BOOTMODS);
-    if ( idx >= MAX_NR_BOOTMODS )
-        panic("module-index=%u of %s overflows MAX_NR_BOOTMODS=%u.\n",
-              idx, name, MAX_NR_BOOTMODS);
+    prop = fdt_get_property(fdt, node, "module-index", NULL);
+    if ( prop )
+    {
+        idx = device_tree_get_u32(fdt, node, "module-index", MAX_NR_BOOTMODS);
+        if ( idx >= MAX_NR_BOOTMODS )
+            panic("module-index=%u of %s overflows MAX_NR_BOOTMODS=%u.\n",
+                  idx, name, MAX_NR_BOOTMODS);
 
+        bi->mods[idx].kind = fdt_node_to_kind(fdt, node);
+
+        printk(XENLOG_DEBUG " %s kind=%s module-index=%u\n",
+               name, boot_module_kind_as_string(bi->mods[idx].kind), idx);
+
+        return 0;
+    }
+
+    /*
+     * Module wasn't passed along with multiboot. Needs
+     * adding to the list if it isn't already
+     */
+
+    prop = fdt_get_property(fdt, node, "reg", &len);
+    if ( !prop )
+        panic("module \"%s\" missing both module-index and reg.\n", name);
+
+    if ( len < dt_cells_to_size(address_cells + size_cells) )
+        panic("malformed reg property of %s", name);
+
+    cell = (const __be32 *)prop->data;
+    device_tree_get_reg(&cell, address_cells, size_cells, &start, &size);
+
+    /* Ensure it's not already in the module list */
+    for ( idx = 0; idx < bi->nr_modules; idx++ )
+        if ( bi->mods[idx].start == start )
+            panic("module overflow. can't add reg-provided module %s.\n", name);
+
+    if ( bi->nr_modules >= MAX_NR_BOOTMODS )
+        panic("builder: module overflow. can't add reg-provided module %s.\n",
+              name);
+
+    bi->mods[idx].start = start;
+    bi->mods[idx].size = size;
     bi->mods[idx].kind = fdt_node_to_kind(fdt, node);
 
-    printk(XENLOG_DEBUG " %s kind=%s module-index=%u\n",
-           name, boot_module_kind_as_string(bi->mods[idx].kind), idx);
+    bi->nr_modules++;
+
+    printk(XENLOG_DEBUG " %s kind=%s start=%#08lx sz=%08lx\n",
+           name, boot_module_kind_as_string(bi->mods[idx].kind),
+           bi->mods[idx].start, bi->mods[idx].size);
 
     return 0;
 }
