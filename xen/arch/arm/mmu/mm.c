566ec8606a4592c51f05e373582edc793d128055
Luca Miccio <luca.miccio@amd.com>
2025-09-22 12:54:57 -0700

    xen/arm: fix frametable_base_pdx underflow for low RAM addresses
    On platforms where RAM starts at a physical address greater than 0 but
    less than PDX_GROUP_COUNT, we currently set frametable_base_pdx to
    directmap_mfn_start. This is true under the assumption that low addresses
    are never compressed by PDX, as compression typically affects higher
    address bits.
    
    However, this causes an underflow in pdx_to_page during the first chunk
    allocation , where we attempt to access pdx_s = 0.
    
    To prevent this, if directmap_mfn_start < PDX_GROUP_COUNT, we should
    set frametable_base_pdx safely to 0.
    
    Fixes: fb10b35 ("xen/arm: Take into account PDX grouping for setting up frametable")
    Signed-off-by: Luca Miccio <luca.miccio@amd.com>
    Reviewed-by: Michal Orzel <michal.orzel@amd.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/mmu/mm.c
+++ xen/arch/arm/mmu/mm.c
@@ -50,7 +50,9 @@ void __init init_frametable(void)
     BUILD_BUG_ON((sizeof(paddr_t) * BITS_PER_BYTE) < PADDR_BITS);
     BUILD_BUG_ON(sizeof(struct page_info) != PAGE_INFO_SIZE);
 
-    frametable_base_pdx = mfn_to_pdx(directmap_mfn_start);
+    frametable_base_pdx = (mfn_x(directmap_mfn_start) < PDX_GROUP_COUNT)
+                          ? 0
+                          : mfn_to_pdx(directmap_mfn_start);
 
     max_pdx = pfn_to_pdx(max_page - 1) + 1;
 
