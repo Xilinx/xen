db52b4b55e44ad6017ab2e52dc563aeb464a25be
Alejandro Vallejo via groups.io <alejandro.garciavallejo=amd.com@groups.io>
2025-09-24 16:18:28 +0200

    dom0less: Make d0 be hwdom-only
    Crash when trying to create a dom0 that's not a hardware domain, or
    a hardware domain that's not dom0.
    
    Fixes: f7cd1c1c5c56("dom0less: Parse the domid property")
    Signed-off-by: Alejandro Vallejo <alejandro.garciavallejo@amd.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/common/device-tree/dom0less-bindings.c
+++ xen/common/device-tree/dom0less-bindings.c
@@ -160,6 +160,10 @@ int __init parse_dom0less_node(struct dt_device_node *node,
     {
         if ( val >= DOMID_FIRST_RESERVED )
             panic("bad domid for node=%s domid=%u\n", dt_node_name(node), val);
+        if ( val && (*flags & CDF_hardware) )
+            panic("hwdom is d%u, but must be d0\n", val);
+        if ( !val && !(*flags & CDF_hardware) )
+            panic("can't create non-hwdom d0");
 
         bd->domid = val;
     }
