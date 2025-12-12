e9eb1686fd1454ded55a41374c32880ad3ee41a0
Penny Zheng <penny.zheng@amd.com>
2025-12-12 12:49:57 +0100

    xen/domid: compile out exhaust search in dom0less mode
    In dom0less mode, there are two domid policies, DEDUCED and EXPLICIT. Dynamic
    domid allocation through exhaust search isnot needed. So we could guard it
    under DOM0LESS_BOOT=n.
    
    Signed-off-by: Penny Zheng <penny.zheng@amd.com>
    Reviewed-by: Michal Orzel <michal.orzel@amd.com>

--- xen/common/domid.c
+++ xen/common/domid.c
@@ -40,12 +40,12 @@ domid_t domid_alloc(domid_t domid)
             domid = DOMID_INVALID;
     }
     /*
-     * Exhaustive search.
+     * Exhaustive search only when DOM0LESS_BOOT=n.
      *
      * Domain ID#0 is reserved for the first boot domain (e.g. control domain)
      * and excluded from allocation.
      */
-    else
+    else if ( !IS_ENABLED(CONFIG_DOM0LESS_BOOT) )
     {
         domid_t bound = DOMID_FIRST_RESERVED;
 
