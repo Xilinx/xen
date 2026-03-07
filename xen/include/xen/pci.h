44dcc6693331a17736fd156f43688b35561c0a75
Luca Fancellu <luca.fancellu@arm.com>
2026-03-06 21:24:30 -0500

    xen/pci: Create pci_assign_device
    Create the public function pci_assign_device(...) to assign devices
    to guests. The function is a wrapper of assign_device and it also
    performs the lock/unlock operations.
    
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/include/xen/pci.h
+++ xen/include/xen/pci.h
@@ -231,6 +231,7 @@ int pci_add_segment(u16 seg);
 const unsigned long *pci_get_ro_map(u16 seg);
 int pci_add_device(struct domain *d, u16 seg, u8 bus, u8 devfn,
                    const struct pci_dev_info *info, nodeid_t node);
+int pci_assign_device(struct domain *d, u16 seg, u8 bus, u8 devfn, u32 flag);
 int pci_remove_device(u16 seg, u8 bus, u8 devfn);
 int pci_ro_device(int seg, int bus, int devfn);
 int pci_hide_device(unsigned int seg, unsigned int bus, unsigned int devfn);
