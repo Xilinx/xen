385443c9f15f5011958ff50353c65730cf92d3bc
Luca Fancellu <luca.fancellu@arm.com>
2026-03-06 21:24:23 -0500

    xen/pci: modify pci_add_device to handle device add by Xen
    Currently pci_add_device is called from hypercalls requested by Dom0
    to add pci devices and when the device has no domain associated with
    it, it is assumed that hardware_domain is the owner.
    
    On the dom0less scenario, the enumeration is performed by the
    firmware and Xen at boot time might want to assign some pci devices
    to guests, so it has to firstly add the device and then assign it to
    the final guest.
    
    Modify pci_add_device to have the owner domain passed as a parameter
    to the function, so that when it is called from the hypercall the
    owner would be the hardware domain, otherwise when Xen is calling it,
    it would be another domain since hw domain could not be there
    (dom0less guests without Dom0 use case).
    
    In pci_add_device there is a call to xsm that doesn't consider the
    requester of the function to be Xen itself, so add a check to skip
    the call if the owner domain is dom_io, since it means the call is
    coming directly from Xen.
    
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Acked-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/include/xen/pci.h
+++ xen/include/xen/pci.h
@@ -227,7 +227,7 @@ void setup_hwdom_pci_devices(struct domain *d,
 int pci_release_devices(struct domain *d);
 int pci_add_segment(u16 seg);
 const unsigned long *pci_get_ro_map(u16 seg);
-int pci_add_device(u16 seg, u8 bus, u8 devfn,
+int pci_add_device(struct domain *d, u16 seg, u8 bus, u8 devfn,
                    const struct pci_dev_info *info, nodeid_t node);
 int pci_remove_device(u16 seg, u8 bus, u8 devfn);
 int pci_ro_device(int seg, int bus, int devfn);
