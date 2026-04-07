Xilinx/xen

commit e93b399f3ad66c1e109530596cdc67cd0b61be54

vcpu: introduce vcpu_state_reset()
Add vcpu_state_reset() function to reinitialize VCPU state during domain
reset. This function:
- resets arch-specific state via arch_vcpu_state_reset()
- resets event channel polling
- resets pause flags
- resets runstate
- resets affinity
- resets timers
- unmaps vcpu info
- unmaps vcpu guest runstate
- resets vcpu waitqueue
- resets vcpu io

Signed-off-by: Xenia Ragiadakou <xenia.ragiadakou@amd.com>
Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/common/domain.c
@@ -1799,6 +1799,70 @@ int vcpu_reset(struct vcpu *v)
     return rc;
 }
 
+void vcpu_state_reset(struct vcpu *v)
+{
+    struct domain *d = v->domain;
+
+    if ( v != current )
+        ASSERT(atomic_read(&v->pause_count));
+    domain_lock(d);
+
+    set_bit(_VPF_in_reset, &v->pause_flags);
+    arch_vcpu_state_reset(v);
+
+    set_bit(_VPF_down, &v->pause_flags);
+
+    clear_bit(v->vcpu_id, d->poll_mask);
+    v->poll_evtchn = 0;
+
+    v->fpu_initialised = 0;
+    v->fpu_dirtied     = 0;
+    v->is_initialised  = 0;
+    v->dirty_cpu = VCPU_CPU_CLEAN;
+
+    v->runstate.state = RUNSTATE_offline;
+    v->runstate.state_entry_time = NOW();
+    memset(v->runstate.time, 0, sizeof(v->runstate.time));
+
+    if ( v->affinity_broken & VCPU_AFFINITY_OVERRIDE )
+        vcpu_temporary_affinity(v, NR_CPUS, VCPU_AFFINITY_OVERRIDE);
+    if ( v->affinity_broken & VCPU_AFFINITY_WAIT )
+        vcpu_temporary_affinity(v, NR_CPUS, VCPU_AFFINITY_WAIT);
+    clear_bit(_VPF_blocked, &v->pause_flags);
+    clear_bit(_VPF_blocked_in_xen, &v->pause_flags);
+    clear_bit(_VPF_mem_paging, &v->pause_flags);
+    clear_bit(_VPF_mem_access, &v->pause_flags);
+    clear_bit(_VPF_mem_sharing, &v->pause_flags);
+
+    stop_timer(&v->periodic_timer);
+    v->periodic_period = 0;
+    v->periodic_last_event = 0;
+    stop_timer(&v->singleshot_timer);
+    stop_timer(&v->poll_timer);
+
+    set_xen_guest_handle(runstate_guest(v), NULL);
+    unmap_guest_area(v, &v->vcpu_info_area);
+    unmap_guest_area(v, &v->runstate_guest_area);
+
+    if ( IS_ENABLED(CONFIG_VM_EVENT) )
+    {
+        reset_waitqueue_vcpu(v);
+        ASSERT(!atomic_read(&v->vm_event_pause_count));
+    }
+
+#ifdef CONFIG_IOREQ_SERVER
+    v->mapcache_invalidate = false;
+    v->io.req.state = STATE_IOREQ_NONE;
+    v->io.completion = VIO_no_completion;
+    v->io.suspended = false;
+#endif
+    v->paused_for_shutdown = 0;
+
+    clear_bit(_VPF_in_reset, &v->pause_flags);
+
+    domain_unlock(d);
+}
+
 int map_guest_area(struct vcpu *v, paddr_t gaddr, unsigned int size,
                    struct guest_area *area,
                    void (*populate)(void *dst, struct vcpu *v))

xen/include/xen/domain.h
@@ -26,6 +26,7 @@ unsigned int dom0_max_vcpus(void);
 int parse_arch_dom0_param(const char *s, const char *e);
 
 int vcpu_reset(struct vcpu *v);
+void vcpu_state_reset(struct vcpu *v);
 int vcpu_up(struct vcpu *v);
 
 void setup_system_domains(void);

commit 2d401d8d43885ffa888e2325c7fa74f6224fc1f7

xen/domain: fix broken assertion in vcpu_state_reset for domain-paused vCPUs
The assertion in vcpu_state_reset() only checks v->pause_count, but
domain_full_reset() pauses guests via domain_pause_except_self()
which increments d->pause_count instead.  This causes a spurious
assertion failure on debug builds even though the vCPU is effectively
not schedulable.

Fix by also accepting d->pause_count > 0 in the assertion, consistent
with the equivalent check already used in unmap_guest_area().

Fixes: e93b399f3ad6 ("vcpu: introduce vcpu_state_reset()")
Signed-off-by: Luca Miccio <luca.miccio@amd.com>
Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/common/domain.c
@@ -1809,7 +1809,7 @@ int vcpu_state_reset(struct vcpu *v)
     int rc;
 
     if ( v != current )
-        ASSERT(atomic_read(&v->pause_count));
+        ASSERT(atomic_read(&v->pause_count) | atomic_read(&d->pause_count));
     domain_lock(d);
 
     set_bit(_VPF_in_reset, &v->pause_flags);