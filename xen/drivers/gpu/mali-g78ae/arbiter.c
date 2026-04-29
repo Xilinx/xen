Xilinx/xen

commit 0ac31fdf188009ba7201bf9ccc0a09e5cd6815a1

xen/drivers: add Mali78AE Arbiter, GSI and scheduling support
This patch introduces the arbiter software component, a key part of
the Mali-G78AE GPU virtualization stack. The arbiter manages GPU
subinstances (GSIs), which represent individual GPU partitions and
are treated as schedulable resources.

Each arbiter instance is responsible for a single GSI and is created
during resource group initialization. It coordinates access to the
GPU among multiple VMs, ensuring fair and exclusive usage through
access windows.

Key responsibilities of the arbiter include:
- Managing GPU kernel driver instances per VM
- Mapping Access Window (AW) IDs to Domain IDs
- Registering and unregistering domains
- Selecting which VM can access the GPU at a given time
- Enforcing fair scheduling across VMs

This commit also introduces the GSI scheduling interface, which
defines how GPU subinstances are scheduled. A null scheduler will
be implemented in future patches to provide baseline scheduling
behavior.

Signed-off-by: Luca Miccio <luca.miccio@amd.com>
Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/drivers/gpu/mali-g78ae/arbiter.h
@@ -1,0 +1,95 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Mali G78AE GPU Arbiter Header
+ *
+ * Copyright (C) 2025, Advanced Micro Devices, Inc.
+ */
+#ifndef DRIVERS__GPU_MALI_G78AE_ARBITER_H
+#define DRIVERS__GPU_MALI_G78AE_ARBITER_H
+
+#include <xen/domain.h>
+#include <xen/list.h>
+#include <xen/spinlock.h>
+#include <xen/types.h>
+
+#include "common.h"
+#include "gpu-subinstance.h"
+#include "resource-group.h"
+
+struct mali_arbiter;
+
+struct mali_vm_data {
+    struct list_head sched_entry;
+    struct list_head entry;
+    unsigned int aw;
+    struct mali_arbiter *arb;
+    bool gpu_lost;
+    struct domain *domain;
+    int gsi_idx;
+};
+
+struct gsi_info {
+    bool enabled;
+    struct mali_arb_gsi *gsi;
+
+    spinlock_t lock;
+    /* AW mask assigned to this GSI */
+    uint16_t aw_mask;
+    /* Slices assigned to this GSI */
+    uint32_t slice_mask;
+};
+
+struct mali_arbiter {
+    /* gpu-subinstance information */
+    struct gsi_info gsi_info[MALI_PTM_PARTITION_COUNT];
+
+    /* Resource group instance managed by this arbiter */
+    struct mali_ptm_rg *rg;
+
+    /* slice information */
+    uint32_t l2_slices;
+    uint32_t core_mask;
+
+    /* vm information */
+    struct list_head wait_list;
+    struct list_head reg_vms_list;
+
+    /* Protect lists */
+    spinlock_t lock;
+};
+
+int mali_arbiter_create(struct mali_arbiter **arb, struct mali_ptm_rg *rg);
+void mali_arbiter_destroy(struct mali_arbiter *arb);
+
+/* Arbiter interface */
+int mali_arbif_register_vm(struct mali_arbiter *arb,
+                           struct mali_vm_data *vm_data);
+void mali_arbif_unregister_vm(struct mali_vm_data *vm_data);
+int mali_arbif_assign_domain(struct mali_arbiter *arb, struct domain *d);
+int mali_arbif_unassign_domain(struct mali_arbiter *arb, struct domain *d);
+
+void mali_arbif_on_gpu_request(struct mali_vm_data *vm_data);
+void mali_arbif_on_gpu_active(struct mali_vm_data *vm_data);
+void mali_arbif_on_gpu_idle(struct mali_vm_data *vm_data);
+void mali_arbif_gpu_stopped(struct mali_vm_data *vm_data,bool req_again);
+
+int mali_arbif_get_max_config(struct mali_vm_data *vm_data,
+                              uint32_t *max_l2_slices, uint32_t *max_core_mask);
+int mali_arbif_get_aw_assignment(struct mali_arbiter *arb, unsigned int gsi_idx,
+                                 uint16_t *aw_mask);
+int mali_arbif_set_aw_assignment(struct mali_arbiter *arb, unsigned int gsi_idx,
+                                 uint16_t new_aw_mask);
+int mali_arbif_gpu_stop(struct mali_vm_data *vm_data);
+int mali_arbif_gpu_granted(struct mali_vm_data *vm_data, uint32_t freq);
+int mali_arbif_gpu_lost(struct mali_vm_data *vm_data);
+
+#endif /* DRIVERS__GPU_MALI_G78AE_ARBITER_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

xen/drivers/gpu/mali-g78ae/gpu-subinstance.c
@@ -1,0 +1,241 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Mali G78AE GPU PTM GPU Subinstance (GSI) handling
+ *
+ * Copyright (C) 2025, Advanced Micro Devices, Inc.
+ */
+#include "gpu-subinstance.h"
+
+int mali_gsi_create(struct mali_arb_gsi **gsi, unsigned int idx,
+                  struct mali_arbiter *arbiter)
+{
+    struct mali_arb_gsi *gsi_instance;
+
+    gsi_instance = xvzalloc(struct mali_arb_gsi);
+    if ( !gsi_instance )
+        return -ENOMEM;
+
+    gsi_instance->arbiter = arbiter;
+    gsi_instance->idx = idx;
+    gsi_instance->flags = 0;
+    gsi_instance->state = STARTED;
+    gsi_instance->part_cfg = arbiter->rg->cfg[idx];
+    gsi_instance->part_ctrl = arbiter->rg->ctrl[idx];
+
+    if ( gsi_instance->part_cfg == NULL || gsi_instance->part_ctrl == NULL )
+    {
+        printk(XENLOG_ERR "GSI%d: Failed to get part_cfg or part_ctrl\n", idx);
+        xvfree(gsi_instance);
+        return -EINVAL;
+    }
+
+    register_gsi_scheduler(gsi_instance);
+    *gsi = gsi_instance;
+    return 0;
+}
+
+void mali_gsi_destroy(struct mali_arb_gsi *gsi)
+{
+    if ( !gsi )
+        return;
+
+    if ( gsi->sched_ops && gsi->sched_ops->sched_destroy )
+        gsi->sched_ops->sched_destroy(gsi->sched_ptr);
+
+    xfree(gsi->sched_ptr);
+    gsi->sched_ptr = NULL;
+    gsi->sched_ops = NULL;
+    xvfree(gsi);
+}
+
+void mali_gsi_start(struct mali_arb_gsi *gsi)
+{
+    if ( gsi->state != STOPPED )
+    {
+        printk(XENLOG_ERR "GPU subinstance is in invalid state %d", gsi->state);
+        return;
+    }
+    if ( !gsi->sched_ptr || !gsi->sched_ops )
+    {
+        printk(XENLOG_ERR "No scheduler defined for GSI %d", gsi->idx);
+        return;
+    }
+
+    gsi->state = STARTING;
+    gsi->sched_ops->sched_start(gsi->sched_ptr);
+    gsi->state = STARTED;
+}
+
+void mali_gsi_stop(struct mali_arb_gsi *gsi)
+{
+    if ( gsi->state != STARTED )
+    {
+        printk(XENLOG_ERR "GPU subinstance is in invalid state %d", gsi->state);
+        return;
+    }
+    if ( !gsi->sched_ptr || !gsi->sched_ops )
+    {
+        printk(XENLOG_ERR "No scheduler defined for GSI %d", gsi->idx);
+        return;
+    }
+
+    gsi->state = STOPPING;
+    gsi->sched_ops->sched_stop(gsi->sched_ptr);
+    gsi->state = STOPPED;
+}
+
+void mali_gsi_get_utilisation(struct mali_arb_gsi *gsi,
+                              uint32_t *gsi_busytime, uint32_t *gsi_totaltime)
+{
+    if ( !gsi_busytime || !gsi_totaltime )
+        return;
+
+    if ( gsi->sched_ops && gsi->sched_ops->sched_get_utilisation )
+        gsi->sched_ops->sched_get_utilisation(gsi->sched_ptr,
+                                               gsi_busytime, gsi_totaltime);
+    else
+    {
+        *gsi_busytime = 0;
+        *gsi_totaltime = 0;
+    }
+}
+
+void mali_gsi_update_freq(struct mali_arb_gsi *gsi,
+                          uint32_t new_freq)
+{
+    printk(XENLOG_WARNING "Unimplemented: mali_gsi_update_freq called\n");
+    return;
+}
+
+void mali_gsi_flag_set(struct mali_arb_gsi *gsi,
+                       enum mali_arb_flags flag)
+{
+    if ( flag >= GSI_FLAG_MAX )
+        return;
+
+    gsi->flags |= (1U << flag);
+
+    return;
+}
+
+void mali_gsi_flag_clear(struct mali_arb_gsi *gsi,
+                         enum mali_arb_flags flag)
+{
+    if ( flag >= GSI_FLAG_MAX )
+        return;
+
+    gsi->flags &= ~(1U << flag);
+
+    return;
+}
+
+bool mali_arbiter_gsi_remove_vm(struct mali_arb_gsi *gsi,
+                                struct mali_vm_data *rem_vm)
+{
+    if ( !gsi || !gsi->sched_ops || !gsi->sched_ops->sched_remove_vm )
+        return false;
+
+    return gsi->sched_ops->sched_remove_vm(gsi->sched_ptr, rem_vm, false);
+}
+
+void mali_gsi_on_gpu_stopped(struct mali_vm_data *arb_vm,
+                             struct mali_arb_gsi *gsi, bool req_again)
+{
+    if ( !gsi->sched_ops || !gsi->sched_ops->sched_remove_vm )
+    {
+        printk(XENLOG_ERR "GSI%u: sched_ops not set in on_gpu_stopped\n",
+               gsi->idx);
+        return;
+    }
+
+    gsi->sched_ops->sched_remove_vm(gsi->sched_ptr, arb_vm, req_again);
+}
+
+void mali_gsi_on_gpu_request(struct mali_vm_data *arb_vm,
+                             struct mali_arb_gsi *gsi)
+{
+    if ( !gsi->sched_ops || !gsi->sched_ops->sched_add_vm )
+    {
+        printk(XENLOG_ERR "GSI%u: sched_ops not set in on_gpu_request\n",
+               gsi->idx);
+        return;
+    }
+
+    gsi->sched_ops->sched_add_vm(gsi->sched_ptr, arb_vm);
+}
+
+void mali_gsi_on_gpu_active(struct mali_vm_data *arb_vm,
+                            struct mali_arb_gsi *gsi)
+{
+    if ( !gsi->sched_ops )
+        return;
+
+    if ( gsi->sched_ops->sched_gpu_active )
+        gsi->sched_ops->sched_gpu_active(gsi->sched_ptr, arb_vm);
+}
+
+void mali_gsi_on_gpu_idle(struct mali_vm_data *arb_vm,
+                          struct mali_arb_gsi *gsi)
+{
+    if ( !gsi->sched_ops || !gsi->sched_ops->sched_stop_idle_vm )
+    {
+        printk(XENLOG_ERR "GSI%u: sched_ops not set in on_gpu_idle\n",
+               gsi->idx);
+        return;
+    }
+
+    gsi->sched_ops->sched_stop_idle_vm(gsi->sched_ptr, arb_vm);
+}
+
+int mali_gsi_handle_gpu_stop(struct mali_vm_data *arb_vm)
+{
+    return mali_arbif_gpu_stop(arb_vm);
+}
+
+int mali_gsi_handle_gpu_granted(struct mali_arb_gsi *gsi,
+                            struct mali_vm_data *arb_vm)
+{
+    if ( !arb_vm )
+    {
+        printk(XENLOG_ERR "GSI %d: Cannot grant GPU to NULL VM\n", gsi->idx);
+        return -EINVAL;
+    }
+
+    if ( gsi->state != STARTED )
+    {
+        printk(XENLOG_ERR "GSI %d: Cannot grant GPU to VM %d, GSI not started\n",
+               gsi->idx, arb_vm->aw);
+        return -EINVAL;
+    }
+
+    if ( gsi->flags & (1U << GSI_FLAG_SLICE_ASSIGNED) )
+    {
+        ctrlif_assign_partition_to_aw(gsi->part_ctrl, arb_vm->aw);
+    }
+    else
+    {
+        printk(XENLOG_ERR "GSI %d: Cannot grant GPU to VM %d, no slice assigned\n",
+               gsi->idx, arb_vm->aw);
+        return -EINVAL;
+    }
+
+    /*
+     * Frequency calculation not implemented yet.
+     * Granting access with default frequency.
+     */
+    return mali_arbif_gpu_granted(arb_vm, GSI_DEFAULT_FREQ);
+}
+
+int mali_gsi_handle_gpu_lost(struct mali_vm_data *arb_vm)
+{
+    return mali_arbif_gpu_lost(arb_vm);
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

xen/drivers/gpu/mali-g78ae/gpu-subinstance.h
@@ -1,0 +1,103 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Mali G78AE GPU GPU Subinstance (GSI) arbiter interface
+ *
+ * Copyright (C) 2025, Advanced Micro Devices, Inc.
+ */
+#ifndef DRIVERS__GPU_MALI_G78AE_ARBITER_GSI_H
+#define DRIVERS__GPU_MALI_G78AE_ARBITER_GSI_H
+
+#include <xen/types.h>
+
+#include "arbiter.h"
+#include "gsi-scheduler-if.h"
+#include "partition-config.h"
+#include "partition-control.h"
+
+struct mali_vm_data;
+struct mali_arbiter;
+
+/* Default frequency to use when granting GPU access to a VM */
+#define GSI_DEFAULT_FREQ (999)
+
+/**
+ * enum mali_arb_flags - gpu-subinstance flags
+ * @GSI_FLAG_SLICE_ASSIGNED: Set when slice is assigned to the gpu-subinstance.
+ * @GSI_FLAG_MAX: Maximum valid index.
+ *
+ * Runtime flags that are set by arbiter-core submodule.
+ */
+enum mali_arb_flags { GSI_FLAG_SLICE_ASSIGNED, GSI_FLAG_MAX };
+
+/**
+ * enum mali_arb_state - gpu-subinstance states
+ * @STARTING: A gpu-subinstance start request is in progress
+ * @STARTED: The gpu-subinstance is started
+ * @STOPPING: A gpu-subinstance stop request is in progress
+ * @STOPPED: The gpu-subinstance is stopped
+ *
+ * Definition of the possible arbiter's states
+ */
+enum mali_arb_state {
+    STARTING,
+    STARTED,
+    STOPPING,
+    STOPPED
+};
+struct mali_arb_gsi {
+    unsigned int idx;
+    unsigned int flags;
+    /* Assign interfaces come from partition control and config */
+    struct mali_ptm_part_cfg *part_cfg;
+    struct mali_ptm_part_ctrl *part_ctrl;
+    /* Callbacks */
+    struct mali_arbiter *arbiter;
+    /* Scheduling */
+    void *sched_ptr;
+    const struct mali_arb_gsi_sched_ops *sched_ops;
+
+    enum mali_arb_state state;
+    uint32_t curr_gpu_freq;
+};
+
+/* GSI interface */
+int mali_gsi_create(struct mali_arb_gsi **gsi, unsigned int idx,
+                  struct mali_arbiter *arbiter);
+void mali_gsi_destroy(struct mali_arb_gsi *gsi);
+void mali_gsi_start(struct mali_arb_gsi *gsi);
+void mali_gsi_stop(struct mali_arb_gsi *gsi);
+void mali_gsi_get_utilisation(struct mali_arb_gsi *gsi,
+                        uint32_t *gsi_busytime, uint32_t *gsi_totaltime);
+void mali_gsi_update_freq(struct mali_arb_gsi *gsi, uint32_t new_freq);
+void mali_gsi_flag_set(struct mali_arb_gsi *gsi, enum mali_arb_flags flag);
+void mali_gsi_flag_clear(struct mali_arb_gsi *gsi, enum mali_arb_flags flag);
+bool mali_arbiter_gsi_remove_vm(struct mali_arb_gsi *gsi,
+                struct mali_vm_data *rem_vm);
+/* GSI VM ARB handlers (see arb-vm-protocol.h VM_ARB_*) */
+void mali_gsi_on_gpu_stopped(struct mali_vm_data *arb_vm,
+                  struct mali_arb_gsi *gsi, bool req_again);
+void mali_gsi_on_gpu_request(struct mali_vm_data *arb_vm,
+                  struct mali_arb_gsi *gsi);
+void mali_gsi_on_gpu_active(struct mali_vm_data *arb_vm,
+                  struct mali_arb_gsi *gsi);
+void mali_gsi_on_gpu_idle(struct mali_vm_data *arb_vm,
+                  struct mali_arb_gsi *gsi);
+
+/* GSI ARB VM handlers (see arb-vm-protocol.h GSI_ARB_VM_*) */
+/* Send a gpu stop message to the VM */
+int mali_gsi_handle_gpu_stop(struct mali_vm_data *arb_vm);
+/* Send a gpu granted message to the VM */
+int mali_gsi_handle_gpu_granted(struct mali_arb_gsi *gsi,
+                            struct mali_vm_data *arb_vm);
+/* Send a gpu lost message to the VM */
+int mali_gsi_handle_gpu_lost(struct mali_vm_data *arb_vm);
+#endif
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

commit 9a0a051a943f555ac650cbf38065d5e71f25c410

xen/drivers/mali-g78ae: fix list corruption and races in arbiter
Each mali_vm_data can appear on both reg_vms_list and wait_list
simultaneously, but the code reuses a single list_head for both,
corrupting whichever list it joins second. Introduce a dedicated
wait_entry list_head for wait_list and update all operations accordingly.

Fix two race conditions in the domain assign/unassign path where
arb->lock was dropped between writing vm_data->domain and handling
errors, letting concurrent callers observe half-initialised state.
Keep the lock held across the full sequences and add a rollback path
for assign errors.

Also add missing return-value checks for get_max_l2_slices() and
get_max_core_mask(), replace silent NULL guards with WARN_ON() in the
GPU request callbacks, and document the lock ordering convention.

Fixes: 0ac31fdf18 ("xen/drivers: add Mali78AE Arbiter, GSI and scheduling support")
Signed-off-by: Luca Miccio <luca.miccio@amd.com>
Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

xen/drivers/gpu/mali-g78ae/arbiter.c
@@ -32,18 +32,34 @@ static bool find_vm_by_aw(struct mali_arbiter *arb, unsigned int aw,
 
 /* Must be called with arbiter lock held */
 static bool vm_is_in_wait_list(struct mali_arbiter *arb,
-                              struct mali_vm_data *vm_data)
+                               struct mali_vm_data *vm_data)
 {
-    return find_vm_by_aw(arb, vm_data->aw, &arb->wait_list, NULL);
+    struct mali_vm_data *curr_vm;
+
+    list_for_each_entry(curr_vm, &arb->wait_list, wait_entry)
+    {
+        if ( curr_vm->aw == vm_data->aw )
+            return true;
+    }
+
+    return false;
 }
 
 /* Must be called with arbiter lock held */
 static bool vm_is_in_reg_list(struct mali_arbiter *arb,
                              struct mali_vm_data *vm_data)
 {
-    return find_vm_by_aw(arb, vm_data->aw, &arb->reg_vms_list, NULL);;
+    return find_vm_by_aw(arb, vm_data->aw, &arb->reg_vms_list, NULL);
 }
 
+/*
+ * Find the GSI index that owns a given AW.
+ *
+ * Locking contract:
+ *   - On success (>= 0): returns with arb->gsi_info[ret].lock HELD.
+ *     The caller MUST call spin_unlock(&arb->gsi_info[ret].lock).
+ *   - On failure (-1): returns with NO lock held.
+ */
 static int gsi_idx_from_aw_locked(struct mali_arbiter *arb, unsigned int aw)
 {
     unsigned int i;
@@ -192,7 +208,7 @@ int mali_arbiter_create(struct mali_arbiter **arbiter, struct mali_ptm_rg *rg)
 
     arb->rg = rg;
 
-    for ( i = 0; i < MALI_PTM_PARTITION_COUNT; i++)
+    for ( i = 0; i < MALI_PTM_PARTITION_COUNT; i++ )
     {
         if ( !(rg->partition_mask & (1U << i)) )
         {
@@ -211,7 +227,6 @@ int mali_arbiter_create(struct mali_arbiter **arbiter, struct mali_ptm_rg *rg)
         if ( err )
             goto clean_instances;
 
-
         err = rgif_get_aw_mask(arb->rg, &arb->gsi_info[i].aw_mask);
         if ( err )
             goto clean_instances;
@@ -223,7 +238,7 @@ int mali_arbiter_create(struct mali_arbiter **arbiter, struct mali_ptm_rg *rg)
         printk(XENLOG_DEBUG "P%u: AW mask=0x%x slices=0x%x\n",
                i, arb->gsi_info[i].aw_mask, arb->gsi_info[i].slice_mask);
 
-        if ( arb->gsi_info[i].slice_mask != 0)
+        if ( arb->gsi_info[i].slice_mask != 0 )
             mali_gsi_flag_set(arb->gsi_info[i].gsi,
                                 GSI_FLAG_SLICE_ASSIGNED);
 
@@ -235,8 +250,12 @@ int mali_arbiter_create(struct mali_arbiter **arbiter, struct mali_ptm_rg *rg)
     spin_lock_init(&arb->lock);
 
     /* Init l2 and core slice values */
-    get_max_l2_slices(arb->rg, &arb->l2_slices);
-    get_max_core_mask(arb->rg, &arb->core_mask);
+    err = get_max_l2_slices(arb->rg, &arb->l2_slices);
+    if ( err )
+        goto clean_instances;
+    err = get_max_core_mask(arb->rg, &arb->core_mask);
+    if ( err )
+        goto clean_instances;
 
     *arbiter = arb;
     return 0;
@@ -258,7 +277,6 @@ void mali_arbiter_destroy(struct mali_arbiter *arb)
         if ( arb->gsi_info[i].enabled )
         {
             mali_gsi_destroy(arb->gsi_info[i].gsi);
-            arb->gsi_info[i].enabled = false;
             arb->gsi_info[i].gsi = NULL;
         }
         arb->gsi_info[i].enabled = false;
@@ -270,8 +288,6 @@ void mali_arbiter_destroy(struct mali_arbiter *arb)
 
 int mali_arbif_register_vm(struct mali_arbiter *arb, struct mali_vm_data *vm_data)
 {
-    int err = 0;
-
     spin_lock(&arb->lock);
     if ( vm_is_in_reg_list(arb, vm_data) )
     {
@@ -279,7 +295,6 @@ int mali_arbif_register_vm(struct mali_arbiter *arb, struct mali_vm_data *vm_dat
         spin_unlock(&arb->lock);
         return -EBUSY;
     }
-    printk(XENLOG_DEBUG "Registering AW: %u\n", vm_data->aw);
 
     vm_data->gpu_lost = false;
     vm_data->arb = arb;
@@ -289,7 +304,7 @@ int mali_arbif_register_vm(struct mali_arbiter *arb, struct mali_vm_data *vm_dat
     list_add_tail(&vm_data->entry, &arb->reg_vms_list);
 
     spin_unlock(&arb->lock);
-    return err;
+    return 0;
 }
 
 int mali_arbif_assign_domain(struct mali_arbiter *arb, struct domain *d)
@@ -302,20 +317,22 @@ int mali_arbif_assign_domain(struct mali_arbiter *arb, struct domain *d)
     if ( !arb || !d )
         return -EINVAL;
 
+    /*
+     * Hold arb->lock across the domain pointer and gsi_idx writes so that
+     * concurrent unassign/assign cannot observe a half-initialised state.
+     */
     spin_lock(&arb->lock);
     if ( !find_vm_by_aw(arb, d->arch.mali_aw, &arb->reg_vms_list, &vm_data) )
     {
-        printk(XENLOG_DEBUG "Domain %u (AW: %u) not found in this RG\n",
-               d->domain_id, d->arch.mali_aw);
         spin_unlock(&arb->lock);
         return -EINVAL;
     }
-    spin_unlock(&arb->lock);
 
     if ( vm_data->domain )
     {
         printk(XENLOG_ERR "AW %u is already assigned to Domain %pd.\n",
                d->arch.mali_aw, vm_data->domain);
+        spin_unlock(&arb->lock);
         return -EBUSY;
     }
 
@@ -326,18 +343,21 @@ int mali_arbif_assign_domain(struct mali_arbiter *arb, struct domain *d)
     if ( gsi_idx < 0 )
     {
         printk(XENLOG_ERR "Failed to find GSI instance for AW %u.\n",
-              vm_data->aw);
+               vm_data->aw);
+        vm_data->domain = NULL;
+        vm_data->gsi_idx = -1;
+        spin_unlock(&arb->lock);
         return gsi_idx;
     }
     vm_data->gsi_idx = gsi_idx;
+    spin_unlock(&arb->lock);
 
-    /* Check if the AW is already assigned to the GSI instance */
     err = mali_arbif_get_aw_assignment(arb, gsi_idx, &aw_mask);
     if ( err )
     {
         printk(XENLOG_ERR "Failed to get AW assignment for AW %u.\n",
                vm_data->aw);
-        return err;
+        goto rollback;
     }
 
     err = mali_arbif_set_aw_assignment(arb, gsi_idx,
@@ -346,12 +366,17 @@ int mali_arbif_assign_domain(struct mali_arbiter *arb, struct domain *d)
     {
         printk(XENLOG_ERR "Failed to set AW assignment for AW %u.\n",
                vm_data->aw);
-        return err;
+        goto rollback;
     }
-    printk(XENLOG_DEBUG "AW mask %x set for AW %u on GSI %d.\n",
-           aw_mask | (1U << d->arch.mali_aw), vm_data->aw, gsi_idx);
 
     return 0;
+
+rollback:
+    spin_lock(&arb->lock);
+    vm_data->domain = NULL;
+    vm_data->gsi_idx = -1;
+    spin_unlock(&arb->lock);
+    return err;
 }
 
 void mali_arbif_unregister_vm(struct mali_vm_data *vm_data)
@@ -364,7 +389,7 @@ void mali_arbif_unregister_vm(struct mali_vm_data *vm_data)
 
     spin_lock(&vm_data->arb->lock);
     if ( vm_is_in_wait_list(vm_data->arb, vm_data) )
-        list_del_init(&vm_data->entry);
+        list_del_init(&vm_data->wait_entry);
 
     if ( vm_is_in_reg_list(vm_data->arb, vm_data) )
         list_del_init(&vm_data->entry);
@@ -388,18 +413,18 @@ int mali_arbif_unassign_domain(struct mali_arbiter *arb, struct domain *d)
         spin_unlock(&arb->lock);
         return -EINVAL;
     }
-    spin_unlock(&arb->lock);
 
     if ( !vm_data->domain )
     {
         printk(XENLOG_ERR "AW %u is not assigned to any domain.\n",
                d->arch.mali_aw);
+        spin_unlock(&arb->lock);
         return -ENOENT;
     }
     printk(XENLOG_DEBUG "Unassigning domain %u from AW %u.\n",
            d->domain_id, vm_data->aw);
-
     vm_data->domain = NULL;
+    spin_unlock(&arb->lock);
 
     return 0;
 }
@@ -407,22 +432,24 @@ int mali_arbif_unassign_domain(struct mali_arbiter *arb, struct domain *d)
 void mali_arbif_on_gpu_request(struct mali_vm_data *vm_data)
 {
     int gsi_idx;
-    struct mali_arbiter *arb = vm_data->arb;
+    struct mali_arbiter *arb;
+
+    if ( WARN_ON(!vm_data || !vm_data->arb) )
+        return;
+
+    arb = vm_data->arb;
 
     gsi_idx = gsi_idx_from_aw_locked(arb, vm_data->aw);
     if ( gsi_idx < 0 )
     {
-        printk(XENLOG_DEBUG "Failed to find GSI instance for AW %u\n",
-               vm_data->aw);
         spin_lock(&arb->lock);
         if ( vm_is_in_wait_list(arb, vm_data) )
         {
             spin_unlock(&arb->lock);
             return;
         }
-        list_add_tail(&vm_data->entry, &arb->wait_list);
+        list_add_tail(&vm_data->wait_entry, &arb->wait_list);
         spin_unlock(&arb->lock);
-        printk(XENLOG_DEBUG "Added AW %u to wait list\n", vm_data->aw);
         return;
     }
 
@@ -435,7 +462,12 @@ void mali_arbif_on_gpu_request(struct mali_vm_data *vm_data)
 void mali_arbif_on_gpu_active(struct mali_vm_data *vm_data)
 {
     int gsi_idx;
-    struct mali_arbiter *arb = vm_data->arb;
+    struct mali_arbiter *arb;
+
+    if ( WARN_ON(!vm_data || !vm_data->arb) )
+        return;
+
+    arb = vm_data->arb;
 
     gsi_idx = gsi_idx_from_aw_locked(arb, vm_data->aw);
     if ( gsi_idx < 0 )
@@ -455,22 +487,22 @@ void mali_arbif_on_gpu_idle(struct mali_vm_data *vm_data)
 {
     int gsi_idx;
     struct mali_vm_data *cur_vm, *tmp_vm;
-    struct mali_arbiter *arb = vm_data->arb;
+    struct mali_arbiter *arb;
+
+    if ( WARN_ON(!vm_data || !vm_data->arb) )
+        return;
+
+    arb = vm_data->arb;
 
     gsi_idx = gsi_idx_from_aw_locked(arb, vm_data->aw);
     if ( gsi_idx < 0 )
     {
-        printk(XENLOG_DEBUG "Failed to find GSI instance for VM %u.\n",
-               vm_data->aw);
-        printk(XENLOG_DEBUG "Remove it from arbiter wait list\n");
         spin_lock(&arb->lock);
-        list_for_each_entry_safe(cur_vm, tmp_vm, &arb->wait_list, entry)
+        list_for_each_entry_safe(cur_vm, tmp_vm, &arb->wait_list, wait_entry)
         {
             if ( cur_vm->aw == vm_data->aw )
             {
-                printk(XENLOG_DEBUG "Removing VM %u from wait list.\n",
-                       vm_data->aw);
-                list_del_init(&cur_vm->entry);
+                list_del_init(&cur_vm->wait_entry);
                 break;
             }
         }
@@ -527,12 +559,17 @@ int mali_arbif_get_max_config(struct mali_vm_data *vm_data,
                               uint32_t *max_l2_slices, uint32_t *max_core_mask)
 {
     struct mali_arbiter *arb = vm_data->arb;
+    int err;
 
     if ( !arb || !max_l2_slices || !max_core_mask )
         return -EINVAL;
 
-    get_max_l2_slices(vm_data->arb->rg, &arb->l2_slices);
-    get_max_core_mask(vm_data->arb->rg, &arb->core_mask);
+    err = get_max_l2_slices(arb->rg, &arb->l2_slices);
+    if ( err )
+        return err;
+    err = get_max_core_mask(arb->rg, &arb->core_mask);
+    if ( err )
+        return err;
 
     *max_l2_slices = arb->l2_slices;
     *max_core_mask = arb->core_mask;
@@ -575,13 +612,12 @@ int mali_arbif_set_aw_assignment(struct mali_arbiter *arb, unsigned int gsi_idx,
         return -EINVAL;
 
     /*
-     * We first acquire the GSI info structure, followed by the arbiter
-     * wait list lock, which we only take when managing the wait list.
-     * Although there is no strict intrinsic locking order required,
-     * it is important to note that we always take the GSI lock first.
-     * This is because the GSI structure is the main data we aim to
-     * protect when calling this function. The wait list lock is taken
-     * only if necessary.
+     * Lock ordering: gsi_info[].lock first, then arb->lock.
+     *
+     * This is consistent with gsi_idx_from_aw_locked() which acquires
+     * gsi_info[].lock without arb->lock. The on_gpu_request/idle/active
+     * paths only acquire arb->lock when gsi_idx_from_aw_locked() fails
+     * (i.e. no gsi_info lock is held), so no ordering conflict arises.
      */
     spin_lock(&arb->gsi_info[gsi_idx].lock);
 
@@ -641,13 +677,11 @@ int mali_arbif_set_aw_assignment(struct mali_arbiter *arb, unsigned int gsi_idx,
      * new gpu-subinstance
      */
     spin_lock(&arb->lock);
-    list_for_each_entry_safe(cur_vm, tmp_vm, &arb->wait_list, entry)
+    list_for_each_entry_safe(cur_vm, tmp_vm, &arb->wait_list, wait_entry)
     {
         if ( (1U << cur_vm->aw) & new_aw_mask )
         {
-            /* Remove the VM from Arbiter wait-queue */
-            list_del_init(&cur_vm->entry);
-            /* Add the VM to new gpu-subinstance */
+            list_del_init(&cur_vm->wait_entry);
             mali_gsi_on_gpu_request(cur_vm, gsi_ptr);
         }
     }
@@ -675,8 +709,7 @@ int mali_arbif_set_aw_assignment(struct mali_arbiter *arb, unsigned int gsi_idx,
                     /* Add the VM to the wait-list if it was in req-list */
                     if ( requested )
                     {
-                        list_del_init(&reg_vm->entry);
-                        list_add_tail(&reg_vm->entry, &arb->wait_list);
+                        list_add_tail(&reg_vm->wait_entry, &arb->wait_list);
                     }
                     break;
                 }
@@ -703,7 +736,7 @@ int mali_arbif_gpu_stop(struct mali_vm_data *vm_data)
     if ( ret )
         return ret;
 
-    ret = ptm_msg_buff_write(&arb->rg->msg_handler.send_msgs,vm_data->aw, message);
+    ret = ptm_msg_buff_write(&arb->rg->msg_handler.send_msgs, vm_data->aw, message);
     if ( ret )
         return ret;
 
@@ -723,7 +756,7 @@ int mali_arbif_gpu_granted(struct mali_vm_data *vm_data, uint32_t freq)
     if ( ret )
         return ret;
 
-    ret = ptm_msg_buff_write(&arb->rg->msg_handler.send_msgs,vm_data->aw, message);
+    ret = ptm_msg_buff_write(&arb->rg->msg_handler.send_msgs, vm_data->aw, message);
     if ( ret )
         return ret;
 
@@ -741,7 +774,7 @@ int mali_arbif_gpu_lost(struct mali_vm_data *vm_data)
         return ret;
 
     printk(XENLOG_INFO "P%u: GPU lost for VM %u.\n", vm_data->aw, vm_data->aw);
-    ret = ptm_msg_buff_write(&arb->rg->msg_handler.send_msgs,vm_data->aw, message);
+    ret = ptm_msg_buff_write(&arb->rg->msg_handler.send_msgs, vm_data->aw, message);
     if ( ret )
         return ret;
     printk(XENLOG_DEBUG "P%u: Sending GPU lost message to VM %u.\n",

xen/drivers/gpu/mali-g78ae/arbiter.h
@@ -21,6 +21,7 @@ struct mali_arbiter;
 struct mali_vm_data {
     struct list_head sched_entry;
     struct list_head entry;
+    struct list_head wait_entry;
     unsigned int aw;
     struct mali_arbiter *arb;
     bool gpu_lost;

xen/drivers/gpu/mali-g78ae/resource-group.c
@@ -797,6 +797,7 @@ static int __init mali_ptm_rg_register_vms(struct mali_ptm_rg *rg)
 
             INIT_LIST_HEAD(&rg->ptm_rg_vm[aw]->sched_entry);
             INIT_LIST_HEAD(&rg->ptm_rg_vm[aw]->entry);
+            INIT_LIST_HEAD(&rg->ptm_rg_vm[aw]->wait_entry);
             rg->ptm_rg_vm[aw]->aw = aw;
             rg->ptm_rg_vm[aw]->gsi_idx = rg->id;
             rg->ptm_rg_vm[aw]->arb = rg->arbiter;